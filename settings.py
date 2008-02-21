872a2c096f731f7fb9771df1a8c01680dad02ebf
Morbus Iff <morbus@disobey.com>
2008-02-21 20:29:50 +0000

    Removing repeated defaults, adding some more docs.

--- settings.py
+++ settings.py
@@ -9,82 +9,52 @@ TEMPLATE_DEBUG = DEBUG
 TEMPLATE_DIRS = ( abspath(join(dirname(__file__), 'templates')), )
 
 # absolute path to the directory that holds media.
+# URL that handles the media served from MEDIA_ROOT.
 MEDIA_ROOT = abspath(join(dirname(__file__), 'media'))
+MEDIA_URL = "http://127.0.0.1:8000/site_media"
 
-
-
-
-
-
-# TODO: Set to True to avoid hitting GCD for every cover image.
-# TODO: Note that if True, the same cover image will be used for
-# TODO: every issue.
+# Set to True to avoid hitting comics.org for every cover image.
+# If True, the same cover image will be used for every issue.
 FAKE_COVER_IMAGES = False
-
-ADMINS = (
-    # ('Your Name', 'your_email@domain.com'),
-)
-
-MANAGERS = ADMINS
-
-DATABASE_ENGINE = 'mysql'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'ado_mssql'.
-
-# TODO: Set the database name, user and password as needed.
-DATABASE_NAME = 'gcd_base'             # Or path to database file if using sqlite3.
-DATABASE_USER = 'gcd_dev'             # Not used with sqlite3.
-DATABASE_PASSWORD = ''         # Not used with sqlite3.
-DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
 
 # Local time zone for this installation. Choices can be found here:
 # http://www.postgresql.org/docs/8.1/static/datetime-keywords.html#DATETIME-TIMEZONE-SET-TABLE
-# although not all variations may be possible on all operating systems.
-# If running in a Windows environment this must be set to the same as your
-# system time zone.
-TIME_ZONE = 'America/Pacific'
+TIME_ZONE = 'GMT'
 
-# Language code for this installation. All choices can be found here:
-# http://www.w3.org/TR/REC-html40/struct/dirlang.html#langcodes
-# http://blogs.law.harvard.edu/tech/stories/storyReader$15
-LANGUAGE_CODE = 'en-us'
+# Database settings. Override yours in a settings.local.py
+# if you're not gonna stick with these development defaults.
+DATABASE_ENGINE   = 'mysql'
+DATABASE_NAME     = 'gcd_base'
+DATABASE_USER     = 'gcd_dev'
+DATABASE_PASSWORD = ''
+DATABASE_HOST     = ''
+DATABASE_PORT     = ''
 
+# The router where all our site URLs is defined.
+ROOT_URLCONF = 'urls'
+
+# Corresponds to the django_site database table. As far
+# as I know, we won't be using this for the GCD.
 SITE_ID = 1
 
-# If you set this to False, Django will make some optimizations so as not
-# to load the internationalization machinery.
-USE_I18N = True
+# Email these if there's a site exception and debug isn't on.
+ADMINS = (
+    ('Henry Andrews', 'andrews_henry at yahoo.com'),
+    ('Morbus Iff', 'morbus@disobey.com'),
+)
+MANAGERS = ADMINS
 
-
-# URL that handles the media served from MEDIA_ROOT.
-# Example: "http://media.lawrence.com"
-MEDIA_URL = "http://127.0.0.1:8000/site_media"
-
-# URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
-# trailing slash.
-# Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/media/'
-
-# Make this unique, and don't share it with anybody.
+# Used to provide a seed in secret-key hashing algorithms.
 SECRET_KEY = 'th0lnu%wjs_8=r4u_km3shvogzd%1n)t-5eosi964g0ek+a4p+'
 
-# List of callables that know how to import templates from various sources.
+# Callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.load_template_source',
     'django.template.loaders.app_directories.load_template_source',
     # 'django.template.loaders.eggs.load_template_source',
 )
 
-MIDDLEWARE_CLASSES = (
-    'django.middleware.common.CommonMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.middleware.doc.XViewMiddleware',
-)
-
-ROOT_URLCONF = 'urls'
-
-
-
+# All enabled apps for this install.
 INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -93,5 +63,4 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     'apps.gcd',
 )
-
 
