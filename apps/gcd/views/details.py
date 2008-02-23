a4d84b746d60506ee3bc2008b1bf7763a84a6b8c
Jochen Garcke <gcdmail@garcke.de>
2008-02-23 23:05:05 +0000

    check if issue is indexed or just a skeleton

--- apps/gcd/views/details.py
+++ apps/gcd/views/details.py
@@ -4,6 +4,8 @@ import re
 from urllib import urlopen
 
 from django.db.models import Q
+from django.core.exceptions import ObjectDoesNotExist
+import django.db.models.base
 from django.conf import settings
 from django.shortcuts import render_to_response, \
                              get_object_or_404, \
@@ -51,8 +53,15 @@ def issue(request, issue_id):
     """Display the issue details page, including story details."""
     issue = get_object_or_404(Issue, id = issue_id)
     image_tag = covers.get_issue_image_tag(issue.series.id, issue_id)
-    cover_story = issue.story_set.get(sequence_number = 0)
-
+    # TODO: we probably can check directly if the issue is indexed
+    try:
+        cover_story = issue.story_set.get(sequence_number = 0)
+    except ObjectDoesNotExist:
+        # create a special empty page (with the cover if existing)
+        return render_to_response('issue.html', {
+          'issue' : issue,
+          'image_tag' : image_tag,
+          'media_url' : settings.MEDIA_URL })
     stories = issue.story_set.filter(sequence_number__gt = 0)
     stories = stories.order_by("sequence_number")
 
