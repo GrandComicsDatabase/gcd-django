8600a806bb1f5a64223b483c4f9bfdd4b2ac1efa
Jochen Garcke <gcdmail@garcke.de>
2009-08-30 20:59:24 +0000

    Pluralize for decimal values, Review 45, Fixes Bug 69

--- apps/gcd/templatetags/mathfilter.py
+++ apps/gcd/templatetags/mathfilter.py
@@ -1,9 +1,14 @@
 # -*- coding: utf-8 -*-
 from django import template
+import math
 
 register = template.Library()
 
 def sub_int(value_1, value_2):
     return int(value_1) - int(value_2)
 
+def ceil(value):
+    return math.ceil(float(value))
+
 register.filter(sub_int)
+register.filter(ceil)
