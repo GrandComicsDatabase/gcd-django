efa6715b1c2d7fe5403b355f144185a5a420c26e
Jochen Garcke <gcdmail@garcke.de>
2009-09-08 18:16:29 +0000

    rev 58, bug 81 mathfilter ceil returns value for non numeric content

--- apps/gcd/templatetags/mathfilter.py
+++ apps/gcd/templatetags/mathfilter.py
@@ -8,7 +8,10 @@ def sub_int(value_1, value_2):
     return int(value_1) - int(value_2)
 
 def ceil(value):
-    return math.ceil(float(value))
+    try:
+        return math.ceil(float(value))
+    except ValueError:
+        return value
 
 register.filter(sub_int)
 register.filter(ceil)
