2133cf28a51f2e57c1843f49d8d03d5e9956edb4
Henry Andrews <hha1@cornell.edu>
2009-09-22 07:22:24 +0000

    Reviewed-by: ralf.haring Approved-by: ralf.haring
    This was left out of the models OI checkin.

---
+++ apps/oi/states.py
@@ -0,0 +1,40 @@
+"""
+This module is just a place to define constants related to index status.
+"""
+
+"""
+Used in old_state field when a reservation is created.
+"""
+UNRESERVED = 99
+
+"""
+Marks an artificial reservation that needs to be replaced when the
+Log* tables are migrated.
+"""
+BASELINE = 0
+
+"""
+The reservation is open and the indexer is working on it.
+"""
+OPEN = 1
+
+"""
+The reservation has been submitted for approval, but is not being examined.
+"""
+PENDING = 2
+
+"""
+The change is being examined for approval.
+"""
+REVIEWING = 4
+
+"""
+Approval has been granted.
+"""
+APPROVED = 5
+
+"""
+Approval was not granted, no further work will be done.
+"""
+DISCARDED = 6
+
