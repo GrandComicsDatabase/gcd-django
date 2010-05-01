e43306cb57f5d502d446471f59e522ba90ea5769
Jochen Garcke <gcdmail@garcke.de>
2010-05-01 08:17:53 +0000

    rev 440, bug 301, 329 show correct last updates issues, optional filter by language

--- apps/gcd/templatetags/statustags.py
+++ apps/gcd/templatetags/statustags.py
@@ -3,22 +3,32 @@ from django import template
 from django.utils.html import conditional_escape as esc
 from django.utils.safestring import mark_safe
 
-from apps.gcd.models import Issue
+from apps.oi.models import IssueRevision, states
 
 register = template.Library()
 
 
 def last_updated_issues(parser, token):
     """
-    Display the <number> last updated indexes as a tag
+    Display the last updated indexes as a tag
+    token is the content of the tag:
+    last_updated_issues <number> language=<language_code>
+    where both number and language are optional
     """
     try:
         number = int(token.split_contents()[1])
     except:
         number = 5
-    issues = Issue.objects.filter(story_type_count__gt=0, reserved=False)\
-				  .order_by('-modified')\
-                                  .select_related('series', 'series__publisher')
+    issues = IssueRevision.objects.filter(issue__story_type_count__gt=0, 
+      changeset__state=states.APPROVED).order_by('-changeset__modified')\
+      .select_related('issue', 'issue__series', 'issue__series__publisher')
+
+    language_pos = token.contents.find('language')
+    if language_pos > 0:
+        language_pos += len('language=')
+        code = token.contents[language_pos:language_pos+2].lower()
+        issues = issues.filter(issue__series__language__code=code)
+
     last_updated_issues = issues[:number]
     return LastUpdatedNode(last_updated_issues)
 
@@ -27,12 +37,15 @@ class LastUpdatedNode(template.Node):
     def __init__(self, issues):
         self.issues = issues
     def render(self, context):
-        return_string = ""
-        for i in self.issues:
-            return_string += "<a href=\"" + i.get_absolute_url() + "\">" + \
-                             esc(i.series) + " #" + esc(i.number) + "</a> (" + \
-                             esc(i.series.publisher.name) + ")<br/><br/>"
-        return mark_safe(return_string)
+        return_string = u''
+        for issue_revision in self.issues:
+            i = issue_revision.issue
+            return_string += u'<a href="%s">%s #%s</a> (%s)<br><br>' % \
+                             (i.get_absolute_url(), esc(i.series),
+                              esc(i.number), esc(i.series.publisher.name))
+        
+        # 8 = len('<br><br>') to cut off last line break
+        return mark_safe(return_string[:-8])
 
 
 register.tag('last_updated_issues', last_updated_issues)
