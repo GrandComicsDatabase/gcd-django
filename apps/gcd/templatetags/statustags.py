5a35d4f8e111324bc7fc2ed255da185c9595ef90
Jochen Garcke <gcdmail@garcke.de>
2010-05-09 10:00:01 +0000

    rev 475 refactor last_updated templatetag

--- apps/gcd/templatetags/statustags.py
+++ apps/gcd/templatetags/statustags.py
@@ -7,6 +7,7 @@ from apps.oi.models import IssueRevision, states
 
 register = template.Library()
 
+LANGUAGES=['de']
 
 def last_updated_issues(parser, token):
     """
@@ -19,26 +20,46 @@ def last_updated_issues(parser, token):
         number = int(token.split_contents()[1])
     except:
         number = 5
-    issues = IssueRevision.objects.filter(issue__story_type_count__gt=0, 
-      changeset__state=states.APPROVED).order_by('-changeset__modified')\
-      .select_related('issue', 'issue__series', 'issue__series__publisher')
 
+    language_in_context = None
+    language_code = None
     language_pos = token.contents.find('language')
     if language_pos > 0:
         language_pos += len('language=')
-        code = token.contents[language_pos:].lower()
-        issues = issues.filter(issue__series__language__code=code)
+        code = token.contents[language_pos:].lower().strip()
+        if code in LANGUAGES: # either a supported language code 
+            language_code = code
+        else: # or the language_code via a template variable
+            language_in_context = code
 
-    last_updated_issues = issues[:number]
-    return LastUpdatedNode(last_updated_issues)
+    return LastUpdatedNode(number, language_code, language_in_context)
 
 
 class LastUpdatedNode(template.Node):
-    def __init__(self, issues):
-        self.issues = issues
+    def __init__(self, number, language_code, language_in_context):
+        if language_in_context:
+            self.language_in_context = template.Variable(language_in_context)
+        else:
+            self.language_in_context = None
+        self.language_code = language_code
+        self.number = number
+
     def render(self, context):
+        if self.language_in_context:
+            try:
+                self.language_code = self.language_in_context.resolve(context)
+            except: # render in templates should fail silently
+                return u''
+
+        issues = IssueRevision.objects.filter(issue__story_type_count__gt=0, 
+          changeset__state=states.APPROVED).order_by('-changeset__modified')\
+          .select_related('issue', 'issue__series', 'issue__series__publisher')
+        if self.language_code:
+            issues = issues.filter(issue__series__language__code=self.language_code)
+ 
+        last_updated_issues = issues[:self.number]
         return_string = u''
-        for issue_revision in self.issues:
+        for issue_revision in last_updated_issues:
             i = issue_revision.issue
             return_string += u'<a href="%s">%s #%s</a> (%s)<br><br>' % \
                              (i.get_absolute_url(), esc(i.series),
