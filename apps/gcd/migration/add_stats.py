e7c82ce2f3f010efed97dd3b4a955ca5b5d9d49e
Jochen Garcke <gcdmail@garcke.de>
2010-07-15 22:12:39 +0000

    stats order fixed

--- apps/gcd/migration/add_stats.py
+++ apps/gcd/migration/add_stats.py
@@ -8,7 +8,7 @@ def add_stats(code):
 #    CountStats.objects.create(name='issues',language=lang,count=Issue.objects.filter(series__language=lang, deleted=False).count())
 #    CountStats.objects.create(name='issues',language=lang,count=Issue.objects.filter(series__language=lang, deleted=False, story_type_count__gt=0).count())
 
-    CountStats.objects.create(name='stories', language=lang,count=Story.objects.filter(issue__series__language=lang, deleted=False).count())
     CountStats.objects.create(name='covers', language=lang,count=Cover.objects.filter(issue__series__language=lang, deleted=False).count())
+    CountStats.objects.create(name='stories', language=lang,count=Story.objects.filter(issue__series__language=lang, deleted=False).count())
 
 
