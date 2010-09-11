217a9f6d82b57cd00f93f642bc4b95429344382c
Henry Andrews <hha1@cornell.edu>
2010-09-11 20:56:54 +0000

    http://reviews.comics.org/r/556/
    Use Django 1.2 features to make the Indexers admin page more useful.  Also,
    use fieldsets to organize the fields.

--- apps/gcd/admin.py
+++ apps/gcd/admin.py
@@ -5,7 +5,27 @@ from apps.gcd.models import Indexer
 class IndexerAdmin(admin.ModelAdmin):
     search_fields = ('user__first_name', 'user__last_name',
                      'user__username', 'user__email')
+    readonly_fields = ('registration_key',)
+    list_display = ('id', 'user', 'registration_expires', 'registration_key')
+    list_display_links = ('id', 'user')
     list_filter = ('is_new', 'registration_expires')
+    list_editable = ('registration_expires',)
+    fieldsets = (
+        (None, {
+            'fields': ('user', 'is_banned',
+                       'registration_key', 'registration_expires'),
+        }),
+        ('Indexing', {
+            'fields': ('mentor', 'is_new', 'max_reservations', 'max_ongoing'),
+        }),
+        ('Profile', {
+            'fields': ('country', 'languages', 'interests', 'deceased'),
+        }),
+        ('Preferences', {
+            'fields': ('notify_on_approve',),
+        }),
+    )
+    filter_horizontal = ('languages',)
 
 admin.site.register(Indexer, IndexerAdmin)
 
