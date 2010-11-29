c6dd04d796f4ca5971ca11d8d6ae382492a5575c
Henry Andrews <hha1@cornell.edu>
2010-11-29 02:57:46 +0000

    Forgot this in last checkin.  grr...

---
+++ apps/gcd/models/recent.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from django.db import models
+from django.conf import settings
+
+class RecentIndexedIssueManager(models.Manager):
+    """
+    Custom manager to allow specialized object creation.
+    """
+
+    def update_recents(self, issue):
+        self.create(issue=issue, language=None)
+        international = self.filter(language__isnull=True)
+        if international.count() >= settings.RECENTS_COUNT:
+            international.order_by('created')[0].delete()
+
+        self.create(issue=issue, language=issue.series.language)
+        local = self.filter(language=issue.series.language)
+        if local.count() >= settings.RECENTS_COUNT:
+            local.order_by('created')[0].delete()
+
+class RecentIndexedIssue(models.Model):
+    """
+    Cache the most recently indexed issues to avoid really expensive
+    scans of the very large oi_changeset and oi_issue_revision tables.
+    """
+    class Meta:
+        app_label = 'gcd'
+        db_table = 'gcd_recent_indexed_issue'
+
+    objects = RecentIndexedIssueManager()
+
+    issue = models.ForeignKey('Issue')
+    language = models.ForeignKey('Language', null=True, db_index=True)
+    created = models.DateTimeField(auto_now_add=True, db_index=True)
+
