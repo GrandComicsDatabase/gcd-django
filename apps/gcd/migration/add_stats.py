946483ac3f6ec194a41b9fff51a98947b6140064
Jochen Garcke <gcdmail@garcke.de>
2010-11-29 12:36:11 +0000

    add_stats with deleted

--- apps/gcd/migration/add_stats.py
+++ apps/gcd/migration/add_stats.py
@@ -3,11 +3,11 @@ from apps.gcd.models import *
 
 def add_stats(code):
     lang = Language.objects.get(code=code)
-    CountStats.objects.create(name='series',language=lang,count=Series.objects.filter(language=lang).count())
-    CountStats.objects.create(name='issues',language=lang,count=Issue.objects.filter(series__language=lang).count())
-    CountStats.objects.create(name='issue indexes',language=lang,count=Issue.objects.filter(series__language=lang, is_indexed=True).count())
-#    CountStats.objects.create(name='issues',language=lang,count=Issue.objects.filter(series__language=lang, deleted=False).count())
-#    CountStats.objects.create(name='issues',language=lang,count=Issue.objects.filter(series__language=lang, deleted=False, is_indexed=True).count())
+    CountStats.objects.create(name='series',language=lang,count=Series.objects.filter(language=lang, deleted=False).count())
+#    CountStats.objects.create(name='issues',language=lang,count=Issue.objects.filter(series__language=lang).count())
+#    CountStats.objects.create(name='issue indexes',language=lang,count=Issue.objects.filter(series__language=lang, is_indexed=True).count())
+    CountStats.objects.create(name='issues',language=lang,count=Issue.objects.filter(series__language=lang, deleted=False).count())
+    CountStats.objects.create(name='issue indexes',language=lang,count=Issue.objects.filter(series__language=lang, deleted=False, is_indexed=True).count())
 
     CountStats.objects.create(name='covers', language=lang,count=Cover.objects.filter(issue__series__language=lang, deleted=False).count())
     CountStats.objects.create(name='stories', language=lang,count=Story.objects.filter(issue__series__language=lang, deleted=False).count())
