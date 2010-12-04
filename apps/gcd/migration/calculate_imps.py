230b88d934812e365d56ea09b1475f2d5fa05fb3
Henry Andrews <hha1@cornell.edu>
2010-12-04 22:29:22 +0000

    http://reviews.comics.org/r/697/
    Preserve modified time when backfilling imps.

--- apps/gcd/migration/calculate_imps.py
+++ apps/gcd/migration/calculate_imps.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
 import sys
 import logging
+
 from django.db import transaction
 from django.db.models import Q
 from django.contrib.auth.models import User
+
 from apps.gcd.models.indexer import Indexer, IMPS_FOR_APPROVAL
+from apps.gcd.migration.history import MigratoryChangeset
+
 from apps.oi import states
 from apps.oi.models import Changeset
 
@@ -23,8 +27,15 @@ def main():
                               .filter(migrated=False):
         if n % 500 == 1:
             logging.info("Calculating changeset %d" % n)
+        mc = MigratoryChangeset.objects.get(id=c.id)
         c.calculate_imps()
+
+        # The MigratoryChangeset will still have the old created and modified
+        # timestamps, and does not have the auto-update code.  It does not
+        # touch the other columns.
         c.save()
+        mc.save()
+
         if c.state == states.APPROVED:
             c.indexer.indexer.add_imps(c.total_imps())
         if c.approver is not None and \
