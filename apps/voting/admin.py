ea091f4ddbe57fb51bb433f368e56e427abde98f
Henry Andrews <hha1@cornell.edu>
2011-04-12 03:17:37 +0000

    http://reviews.comics.org/r/796/
    Update the voting admin as we now have many more objects to manage.

--- apps/voting/admin.py
+++ apps/voting/admin.py
@@ -9,20 +9,24 @@ class OptionInline(admin.StackedInline):
 class TopicAdmin(admin.ModelAdmin):
     inlines = (OptionInline,)
     raw_id_fields = ('author', 'second')
+    list_filter = ('agenda', 'open')
+    list_display = ('name', 'agenda', 'open', 'deadline')
 
 class AgendaMailingListInline(admin.StackedInline):
     model = AgendaMailingList
     extra = 0
 
-class AgendaItemInline(admin.StackedInline):
-    model = AgendaItem
-    extra = 0
-    raw_id_fields = ('owner',)
-
 class AgendaAdmin(admin.ModelAdmin):
-    inlines = (AgendaMailingListInline, AgendaItemInline)
-    
+    inlines = (AgendaMailingListInline,)
+
+class AgendaItemAdmin(admin.ModelAdmin):
+    raw_id_fields = ('owner',)
+    search_fields = ('owner__username', 'owner__first_name', 'owner__last_name')
+    list_display = ('name', 'state', 'owner')
+    list_filter = ('agenda', 'state')
+
 admin.site.register(Agenda, AgendaAdmin)
+admin.site.register(AgendaItem, AgendaItemAdmin)
 admin.site.register(MailingList)
 admin.site.register(Topic, TopicAdmin)
 admin.site.register(VoteType)
