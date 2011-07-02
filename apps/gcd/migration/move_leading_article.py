c2f8e106ed896cf189d3112efcbca8f714e5d719
Jochen Garcke <gcdmail@garcke.de>
2011-07-02 07:23:28 +0000

    update leading articles move with more of them

--- apps/gcd/migration/move_leading_article.py
+++ apps/gcd/migration/move_leading_article.py
@@ -12,7 +12,7 @@ def move_leading_article(article):
 
     series = Series.objects.filter(name__endswith=", %s" % article, reserved=False)
 
-    for s in series[:200]:
+    for s in series[:250]:
         c = migrate_reserve(s, 'series', 'for moving ", %s" to the beginning of the series name' % article)
         if c:
             sr = c.seriesrevisions.all()[0]
@@ -25,11 +25,72 @@ def move_leading_article(article):
 
 @transaction.commit_on_success
 def main():
-    move_leading_article('The')
+    for i in range(18):
+        move_leading_article('The')
     move_leading_article('An')
     move_leading_article('A')
     move_leading_article('Die')
+    move_leading_article('Der')
+    move_leading_article('Ein')
+    move_leading_article('Das')
+    move_leading_article('Los')
+    move_leading_article('La')
+    move_leading_article('Le')
+    move_leading_article('Lo')
+    move_leading_article('Las')
+    move_leading_article('Les')
+    move_leading_article('El')
+    move_leading_article('Un')
+    move_leading_article('Una')
+    move_leading_article('Unos')
+    move_leading_article('Unas')
+    move_leading_article('Il')
+    move_leading_article('I')
+    move_leading_article('Gli')
+    move_leading_article('En')
+    move_leading_article('Et')
+    move_leading_article('Ett')
+    move_leading_article('De')
+    move_leading_article('Het')
+    move_leading_article('Een')
+    move_leading_article("'t")
+    move_leading_article("'n")
 
 if __name__ == '__main__':
     main()
 
+#Spanish
+
+#El
+#La
+#Lo
+#Los
+#Las
+#Un
+#Una
+#Unos
+#Unas
+
+
+#Italian:
+
+#il
+#lo
+#la
+#i
+#gli
+#le
+
+#Norwegian, Danish, Swedish
+
+#En
+#Et
+#Ett
+
+#Dutch:
+#De
+#Het
+#Een
+#D' (not sure if present in our db, but series exist)
+#'t
+#'n
