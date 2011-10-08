3b8bbe7d30dedd6570bed4a4b98c946df958b2a0
Jochen Garcke <gcdmail@garcke.de>
2011-10-08 08:40:25 +0000

    distinct issues needed

--- apps/gcd/migration/fix_reprint_notes.py
+++ apps/gcd/migration/fix_reprint_notes.py
@@ -28,42 +28,50 @@ def fix_reprint_notes(issues, old_reprint_note, new_reprint_note):
 def main():
     old_reprint_note = '100 Bullets: A Foregone Tomorrow (DC, 2002 series) #nn'
     new_reprint_note = '100 Bullets (DC, 2000 series) #4 - A Foregone Tomorrow'
-    issues=Issue.objects.filter(story__reprint_notes__icontains=old_reprint_note, story__deleted=False).filter(deleted=False)
+    issues=Issue.objects.filter(story__reprint_notes__icontains=old_reprint_note,
+                                story__deleted=False).filter(deleted=False).distinct()
     fix_reprint_notes(issues, old_reprint_note, new_reprint_note)
 
     old_reprint_note = '100 Bullets: Decayed (DC, 2006 series) #[nn]'
     new_reprint_note = '100 Bullets (DC, 2000 series) #10 - Decayed'
-    issues=Issue.objects.filter(story__reprint_notes__icontains=old_reprint_note, story__deleted=False).filter(deleted=False)
+    issues=Issue.objects.filter(story__reprint_notes__icontains=old_reprint_note,
+                                story__deleted=False).filter(deleted=False).distinct()
     fix_reprint_notes(issues, old_reprint_note, new_reprint_note)
 
     old_reprint_note = '100 Bullets: Once Upon a Crime (DC, 2007 series) #[nn]'
     new_reprint_note = '100 Bullets (DC, 2000 series) #11 - Once Upon a Crime'
-    issues=Issue.objects.filter(story__reprint_notes__icontains=old_reprint_note, story__deleted=False).filter(deleted=False)
+    issues=Issue.objects.filter(story__reprint_notes__icontains=old_reprint_note,
+                                story__deleted=False).filter(deleted=False).distinct()
     fix_reprint_notes(issues, old_reprint_note, new_reprint_note)
 
     old_reprint_note = '100 Bullets: Dirty (DC, 2008 series) #[nn]'
     new_reprint_note = '100 Bullets (DC, 2000 series) #12 - Dirty'
-    issues=Issue.objects.filter(story__reprint_notes__icontains=old_reprint_note, story__deleted=False).filter(deleted=False)
+    issues=Issue.objects.filter(story__reprint_notes__icontains=old_reprint_note,
+                                story__deleted=False).filter(deleted=False).distinct()
     fix_reprint_notes(issues, old_reprint_note, new_reprint_note)
 
     old_reprint_note = '100 Bullets: Wilt (DC, 2008 series) #[nn]'
     new_reprint_note = '100 Bullets (DC, 2000 series) #13 - Wilt'
-    issues=Issue.objects.filter(story__reprint_notes__icontains=old_reprint_note, story__deleted=False).filter(deleted=False)
+    issues=Issue.objects.filter(story__reprint_notes__icontains=old_reprint_note,
+                                story__deleted=False).filter(deleted=False).distinct()
     fix_reprint_notes(issues, old_reprint_note, new_reprint_note)
 
     old_reprint_note = 'Starman: Sins of the Father (DC, 1995 series) #[nn]'
     new_reprint_note = 'Starman (DC, 1995 series) #1 - Sins of the Father'
-    issues=Issue.objects.filter(story__reprint_notes__icontains=old_reprint_note, story__deleted=False).filter(deleted=False)
+    issues=Issue.objects.filter(story__reprint_notes__icontains=old_reprint_note,
+                                story__deleted=False).filter(deleted=False).distinct()
     fix_reprint_notes(issues, old_reprint_note, new_reprint_note)
 
     old_reprint_note = 'American Century: Scars and Stripes (DC, 2001 series) #[nn]'
     new_reprint_note = 'American Century (DC, 2001 series) #1 - Scars and Stripes'
-    issues=Issue.objects.filter(story__reprint_notes__icontains=old_reprint_note, story__deleted=False).filter(deleted=False)
+    issues=Issue.objects.filter(story__reprint_notes__icontains=old_reprint_note,
+                                story__deleted=False).filter(deleted=False).distinct()
     fix_reprint_notes(issues, old_reprint_note, new_reprint_note)
 
     old_reprint_note = 'American Century: Hollywood Babylon (DC, 2002 series) #[nn]'
     new_reprint_note = 'American Century (DC, 2001 series) #2 - Hollywood Babylon'
-    issues=Issue.objects.filter(story__reprint_notes__icontains=old_reprint_note, story__deleted=False).filter(deleted=False)
+    issues=Issue.objects.filter(story__reprint_notes__icontains=old_reprint_note,
+                                story__deleted=False).filter(deleted=False).distinct()
     fix_reprint_notes(issues, old_reprint_note, new_reprint_note)
 
 
