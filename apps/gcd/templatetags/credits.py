ed36849cd5661d5d3ac06263eff070613afcb2dd
Jochen Garcke <gcdmail@garcke.de>
2011-11-03 21:46:40 +0000

    rev 939 display of new reprint links

--- apps/gcd/templatetags/credits.py
+++ apps/gcd/templatetags/credits.py
@@ -285,6 +285,127 @@ def show_title(story):
         return u'[%s]' % story.title
     return story.title
 
+def generate_reprint_link(issue, from_to, notes=None, li=True):
+    ''' generate reprint link to_issue'''
+
+    link = u'%s <a href="%s">%s</a>' % (from_to, issue.get_absolute_url(),
+                                        esc(issue.full_name()) )
+
+    if notes:
+        link = '%s [%s]' % (link, esc(notes))
+    if issue.publication_date:
+        link += " (" + esc(issue.publication_date) + ")"
+    if li:
+        return '<li> ' + link + ' </li>'
+    else:
+        return link
+
+
+def generate_reprint_link_sequence(story, from_to, notes=None, li=True):
+    ''' generate reprint link to story'''
+
+    link = u'%s <a href="%s#%d">%s</a>' % (from_to, story.issue.get_absolute_url(),
+                                           story.id, esc(story.issue.full_name()) )
+    if notes:
+        link = '%s [%s]' % (link, esc(notes))
+    if story.issue.publication_date:
+        link += " (" + esc(story.issue.publication_date) + ")"
+    if li:
+        return '<li> ' + link + ' </li>'
+    else:
+        return link
+
+# stuff to consider in the display
+# - sort the reprints according to keydate
+# - sort domestic/foreign reprints
+
+def show_reprints(story, original = False):
+    """ Filter for our reprint line on the story level."""
+
+    reprint = ""
+
+    for from_reprint in story.from_reprints.all():
+        reprint += generate_reprint_link_sequence(from_reprint.source,
+                                                  "from ",
+                                                  notes = from_reprint.notes)
+    for to_reprint in story.to_reprints.all():
+        reprint += generate_reprint_link_sequence(to_reprint.target,
+                                                  "in ",
+                                                  notes = to_reprint.notes)
+    for to_reprint in story.to_issue_reprints.all():
+        reprint += generate_reprint_link(to_reprint.target_issue,
+                                         "in ",
+                                         notes = to_reprint.notes)
+    for from_reprint in story.from_issue_reprints.all():
+        reprint += generate_reprint_link(from_reprint.source_issue,
+                                         "from ",
+                                         notes = from_reprint.notes)
+
+    if story.reprint_notes:
+        for string in split_reprint_string(story.reprint_notes):
+            string = string.strip()
+            reprint += '<li> ' + esc(string) + ' </li>'
+    if reprint != '' or (original and story.migration_status.reprint_original_notes):
+        label = _('Reprints') + ': '
+        if original:
+            if not story.migration_status.reprint_confirmed and \
+              story.migration_status.reprint_original_notes:
+                reprint += '</ul></span></dd>' + \
+                  '<dt class="credit_tag">' + '<span class="credit_label">' + \
+                  'Reprint Note before Migration: </span></dt>' + \
+                  '<dd class="credit_def"><span class="credit_value">' + \
+                  story.migration_status.reprint_original_notes + '</dd></span>'
+        else:
+            if not story.migration_status.reprint_confirmed:
+                label += '<span class="linkify">' + \
+                        '<a href="?original_reprints=True">' + \
+                        'show reprint note before migration</a></span>'
+            if story.migration_status.reprint_needs_inspection:
+                label += ' (migrated reprint links need inspection)'
+
+        return mark_safe('<dt class="credit_tag">' + \
+                         '<span class="credit_label">' + label + '</span></dt>' + \
+                         '<dd class="credit_def">' + \
+                         '<span class="credit_value">' + \
+                         '<ul>' + reprint + '</ul></span></dd>')
+    else:
+        return ""
+
+def show_reprints_for_issue(issue):
+    """ show reprints stored on the issue level. """
+
+    reprint = ""
+    if issue.from_reprints.count() > 0:
+        for from_reprint in issue.from_reprints.all():
+            reprint += generate_reprint_link_sequence(from_reprint.source,
+                                                    "from ",
+                                                    notes = from_reprint.notes)
+    if issue.from_issue_reprints.count() > 0:
+        for from_reprint in issue.from_issue_reprints.all():
+            reprint += generate_reprint_link(from_reprint.source_issue,
+                                             "from ",
+                                             notes = from_reprint.notes)
+    if issue.to_reprints.count() > 0:
+        for to_reprint in issue.to_reprints.all():
+            reprint += generate_reprint_link_sequence(to_reprint.target,
+                                                    "in ",
+                                                    notes = to_reprint.notes)
+    if issue.to_issue_reprints.count() > 0:
+        for to_reprint in issue.to_issue_reprints.all():
+            reprint += generate_reprint_link(to_reprint.target_issue,
+                                             "in ",
+                                             notes = to_reprint.notes)
+
+    if reprint != '':
+        label = _('Parts of this issue are reprinted') + ': '
+        dt = '<dt class="credit_tag>'
+        dd = '<dd class="credit_def>'
+
+        return mark_safe(dt + '<span class="credit_label">' + label + '</span></dt>' + \
+               dd + '<span class="credit_value"><ul>' + reprint + '</ul></span></dd>')
+    else:
+        return ""
+
 register.filter(show_credit)
 register.filter(show_credit_status)
 register.filter(show_country)
@@ -294,3 +415,5 @@ register.filter(show_page_count)
 register.filter(format_page_count)
 register.filter(show_title)
 register.filter(show_cover_contributor)
+register.filter(show_reprints)
+register.filter(show_reprints_for_issue)
