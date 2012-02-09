091e280d5c6050963dd5ca0bb7a3d30c7aadd750
Jochen Garcke <gcdmail@garcke.de>
2012-02-09 22:06:37 +0000

    fix typo

--- apps/gcd/migration/fix_reprint_notes.py
+++ apps/gcd/migration/fix_reprint_notes.py
@@ -191,7 +191,7 @@ def main():
     ['Lucky Luke (Dupuis Publishing, 1949 series)', uni('Lucky Luke (Editions Dupuis, 1949 series)')],
     ['Vill Vest  (Se-Bladene, 1957 series)', 'Vill Vest (Se-Bladene, 1955 series)'],
     ['; o; ', '; '],
-    [uni('from (À Suivre) (Casterman', uni('from À Suivre (Casterman')],
+    [uni('from (À Suivre) (Casterman'), uni('from À Suivre (Casterman')],
     ]
     for [old, new] in reprint_notes:
         fix_reprint_notes_global(old, new)
