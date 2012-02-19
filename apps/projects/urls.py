06faaa1a49481d707deb2425193429d4d9b5695c
Jochen Garcke <gcdmail@garcke.de>
2012-02-19 11:47:20 +0000

    remove series with isbn

--- apps/projects/urls.py
+++ apps/projects/urls.py
@@ -10,8 +10,6 @@ urlpatterns = patterns('',
         name='issues_with_several_covers'),
     url(r'^issue_cover_notes/$', 'apps.projects.views.issue_cover_notes',
         name='issue_cover_notes'),
-    url(r'^series_with_isbn/$', 'apps.projects.views.series_with_isbn',
-        name='series_with_isbn'),
     url('$',  direct_to_template,
         { 'template': 'projects/index.html' }, name='projects_toc'),
 )
