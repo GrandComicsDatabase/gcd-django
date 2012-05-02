0a76e64db0fc5c9ee2641554a2a9ad65bf5952ac
Jochen Garcke <gcdmail@garcke.de>
2012-05-02 19:58:56 +0000

    fix reprint source reserved check for newly added ones

--- apps/gcd/templatetags/display.py
+++ apps/gcd/templatetags/display.py
@@ -563,7 +563,7 @@ def compare_current_reprints(object_type, changeset):
                   reprint.next_revision.changeset.modified <= changeset.modified)):
                 kept_string = '%s<li>%s' % (kept_string,
                   reprint.get_compare_string(object_type.issue))
-                if reprint.source.reserved:
+                if reprint.source and reprint.source.reserved:
                     kept_string += ' (reserved in a different changeset)'
                 kept_string += '</li>'
         if kept_string != '':
