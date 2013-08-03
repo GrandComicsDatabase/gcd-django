7825b0e7f15bdbec777cf1b43d34f10b4e344b9d
wkarpeta <wkarpeta@gmail.com>
2013-08-04 01:28:39 +0200

    Not indexing deleted objects. Corrections for index updating.

--- apps/gcd/search_indexes.py
+++ apps/gcd/search_indexes.py
@@ -9,10 +9,12 @@ class IssueIndex(indexes.SearchIndex, indexes.Indexable):
     def get_model(self):
         return Issue
 
-    #TODO This method is commented out as a reminder that it can be needed later
-    # def index_queryset(self, using=None):
-    #     """Used when the entire index for model is updated."""
-    #     return self.get_model().objects.filter(pub_date__lte=datetime.datetime.now())
+    def index_queryset(self, using=None):
+        """Used when the entire index for model is updated."""
+        return self.get_model().objects.filter(deleted=False)
+
+    def get_updated_field(self):
+        return "modified";
 
 
 class SeriesIndex(indexes.SearchIndex, indexes.Indexable):
@@ -21,11 +23,27 @@ class SeriesIndex(indexes.SearchIndex, indexes.Indexable):
     def get_model(self):
         return Series
 
+    def index_queryset(self, using=None):
+        """Used when the entire index for model is updated."""
+        return self.get_model().objects.filter(deleted=False)
+
+    def get_updated_field(self):
+        return "modified";
+
+
 class StoryIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
 
     def get_model(self):
         return Story
+
+    def index_queryset(self, using=None):
+        """Used when the entire index for model is updated."""
+        return self.get_model().objects.filter(deleted=False)
+
+    def get_updated_field(self):
+        return "modified";
+
 
 class PublisherIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True,
@@ -36,6 +54,14 @@ class PublisherIndex(indexes.SearchIndex, indexes.Indexable):
     def get_model(self):
         return Publisher
 
+    def index_queryset(self, using=None):
+        """Used when the entire index for model is updated."""
+        return self.get_model().objects.filter(deleted=False)
+
+    def get_updated_field(self):
+        return "modified";
+
+
 class IndiciaPublisherIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True,
                              use_template=True,
@@ -44,6 +70,14 @@ class IndiciaPublisherIndex(indexes.SearchIndex, indexes.Indexable):
 
     def get_model(self):
         return IndiciaPublisher
+
+    def index_queryset(self, using=None):
+        """Used when the entire index for model is updated."""
+        return self.get_model().objects.filter(deleted=False)
+
+    def get_updated_field(self):
+        return "modified";
+
 
 class BrandIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True,
@@ -54,3 +88,9 @@ class BrandIndex(indexes.SearchIndex, indexes.Indexable):
     def get_model(self):
         return Brand
 
+    def index_queryset(self, using=None):
+        """Used when the entire index for model is updated."""
+        return self.get_model().objects.filter(deleted=False)
+
+    def get_updated_field(self):
+        return "modified";
