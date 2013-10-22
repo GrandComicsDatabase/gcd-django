39655ce91643017ed83390fded6b4261872fe517
Alexandros Diamantidis <adia@hellug.gr>
2013-10-22 18:15:41 +0300

    Changes for django-imagekit >= 3.0
    http://reviews.comics.org/r/1191/

--- apps/gcd/models/image.py
+++ apps/gcd/models/image.py
@@ -41,9 +41,9 @@ class Image(models.Model):
     type = models.ForeignKey(ImageType)
 
     image_file = models.ImageField(upload_to=get_generic_image_path)
-    scaled_image = ImageSpecField([ResizeToFit(width=400),], image_field='image_file',
+    scaled_image = ImageSpecField([ResizeToFit(width=400),], source='image_file',
             format='JPEG', options={'quality': 90})
-    thumbnail = ImageSpecField([ResizeToFit(height=50),], image_field='image_file',
+    thumbnail = ImageSpecField([ResizeToFit(height=50),], source='image_file',
             format='JPEG', options={'quality': 90})
 
     marked = models.BooleanField(default=False)
