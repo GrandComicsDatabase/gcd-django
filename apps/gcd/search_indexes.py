a12c00830a4a116061c29eb6f03ad2a18710a076
wkarpeta <wkarpeta@gmail.com>
2013-12-01 03:40:22 +0100

    Some refactorings.

--- apps/gcd/search_indexes.py
+++ apps/gcd/search_indexes.py
@@ -3,118 +3,86 @@ from apps.gcd.models import Issue, Series, Story, Publisher, IndiciaPublisher,\
     Brand
 
 
-class IssueIndex(indexes.SearchIndex, indexes.Indexable):
+class ObjectIndex(object):
+    def index_queryset(self, using=None):
+        """Used when the entire index for model is updated."""
+        return self.get_model().objects.filter(deleted=False)
+
+    def get_updated_field(self):
+        return "modified"
+
+
+class IssueIndex(ObjectIndex, indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
     facet_model_name = indexes.CharField(faceted=True)
 
     def get_model(self):
         return Issue
 
-    def index_queryset(self, using=None):
-        """Used when the entire index for model is updated."""
-        return self.get_model().objects.filter(deleted=False)
-
-    def get_updated_field(self):
-        return "modified"
-
     def prepare_facet_model_name(self, obj):
         return "issue"
 
 
-class SeriesIndex(indexes.SearchIndex, indexes.Indexable):
+class SeriesIndex(ObjectIndex, indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
     facet_model_name = indexes.CharField(faceted=True)
 
     def get_model(self):
         return Series
 
-    def index_queryset(self, using=None):
-        """Used when the entire index for model is updated."""
-        return self.get_model().objects.filter(deleted=False)
-
-    def get_updated_field(self):
-        return "modified"
-
     def prepare_facet_model_name(self, obj):
         return "series"
 
 
-class StoryIndex(indexes.SearchIndex, indexes.Indexable):
+class StoryIndex(ObjectIndex, indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
     facet_model_name = indexes.CharField(faceted=True)
 
     def get_model(self):
         return Story
 
-    def index_queryset(self, using=None):
-        """Used when the entire index for model is updated."""
-        return self.get_model().objects.filter(deleted=False)
-
-    def get_updated_field(self):
-        return "modified"
-
     def prepare_facet_model_name(self, obj):
         return "story"
 
 
-class PublisherIndex(indexes.SearchIndex, indexes.Indexable):
+class PublisherIndex(ObjectIndex, indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True,
                              use_template=True,
                              template_name=
-                               'search/indexes/gcd/publisher_text.txt')
+                             'search/indexes/gcd/publisher_text.txt')
     facet_model_name = indexes.CharField(faceted=True)
 
     def get_model(self):
         return Publisher
 
-    def index_queryset(self, using=None):
-        """Used when the entire index for model is updated."""
-        return self.get_model().objects.filter(deleted=False)
-
-    def get_updated_field(self):
-        return "modified"
-
     def prepare_facet_model_name(self, obj):
         return "publisher"
 
 
-class IndiciaPublisherIndex(indexes.SearchIndex, indexes.Indexable):
+class IndiciaPublisherIndex(ObjectIndex, indexes.SearchIndex,
+                            indexes.Indexable):
     text = indexes.CharField(document=True,
                              use_template=True,
                              template_name=
-                               'search/indexes/gcd/publisher_text.txt')
+                             'search/indexes/gcd/publisher_text.txt')
     facet_model_name = indexes.CharField(faceted=True)
 
     def get_model(self):
         return IndiciaPublisher
 
-    def index_queryset(self, using=None):
-        """Used when the entire index for model is updated."""
-        return self.get_model().objects.filter(deleted=False)
-
-    def get_updated_field(self):
-        return "modified"
-
     def prepare_facet_model_name(self, obj):
-        return "indicia_publisher"
+        return "indicia publisher"
 
 
-class BrandIndex(indexes.SearchIndex, indexes.Indexable):
+class BrandIndex(ObjectIndex, indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True,
                              use_template=True,
                              template_name=
-                               'search/indexes/gcd/publisher_text.txt')
+                             'search/indexes/gcd/publisher_text.txt')
     facet_model_name = indexes.CharField(faceted=True)
 
     def get_model(self):
         return Brand
-
-    def index_queryset(self, using=None):
-        """Used when the entire index for model is updated."""
-        return self.get_model().objects.filter(deleted=False)
-
-    def get_updated_field(self):
-        return "modified"
 
     def prepare_facet_model_name(self, obj):
         return "brand"
