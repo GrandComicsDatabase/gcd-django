bbaaf331f32e7781bbb6853b230dd3e654ed37a4
Jochen Garcke <gcdmail@garcke.de>
2014-07-12 12:39:09 +0200

    improve new search behaviour

--- apps/gcd/views/search_haystack.py
+++ apps/gcd/views/search_haystack.py
@@ -102,10 +102,18 @@ class PaginatedFacetedSearchView(FacetedSearchView):
             self.form.searchqueryset = self.form.searchqueryset.exclude(not_sq)
 
         self.results = self.get_results()
-        self.sort = ''
-        if len(self.form.selected_facets) == 1:
-            if 'sort' in request.GET:
-                self.sort = request.GET['sort']
+        if 'sort' in request.GET:
+            self.sort = request.GET['sort']
+        else:
+            self.sort = ''
+        if self.sort == 'country':
+            self.results = self.results.order_by('country',
+                                                 '-_score')
+        elif self.sort == 'year':
+            self.results = self.results.order_by('year',
+                                                 '-_score')
+        elif len(self.form.selected_facets) == 1:
+            if self.sort:
                 if self.form.selected_facets[0] in \
                   [u'facet_model_name_exact:publisher',
                    u'facet_model_name_exact:indicia publisher',
@@ -114,9 +122,9 @@ class PaginatedFacetedSearchView(FacetedSearchView):
                    u'facet_model_name_exact:series']:
                     if request.GET['sort'] == 'alpha':
                         self.results = self.results.order_by('sort_name',
-                                                             'year_began')
+                                                             'year')
                     elif request.GET['sort'] == 'chrono':
-                        self.results = self.results.order_by('year_began',
+                        self.results = self.results.order_by('year',
                                                              'sort_name')
                 elif u'facet_model_name_exact:issue' in self.form.selected_facets:
                     if request.GET['sort'] == 'alpha':
