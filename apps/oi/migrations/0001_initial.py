a92253441addb0bfaa0d08bb19656cd00f96bc4e
handrews <hha1@cornell.edu>
2015-11-06 00:44:16 -0800

    Add initial data, dependencies.

--- apps/oi/migrations/0001_initial.py
+++ apps/oi/migrations/0001_initial.py
@@ -7,6 +7,10 @@ from django.db import models
 
 class Migration(SchemaMigration):
 
+    depends_on = (
+        ("gcd", "0001_initial"),
+    )
+
     def forwards(self, orm):
         # Adding model 'Changeset'
         db.create_table(u'oi_changeset', (
