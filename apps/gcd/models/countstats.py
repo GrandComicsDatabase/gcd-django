77a148837b1715a5f03ef4884f6fb31e07523e95
handrews <hha1@cornell.edu>
2015-12-11 20:05:08 -0800

    Make counstats.py pass flake8

--- apps/gcd/models/countstats.py
+++ apps/gcd/models/countstats.py
@@ -25,18 +25,20 @@ class CountStatsManager(models.Manager):
 
         if language is None:
             self.create(name='publishers', country=country,
-              count=Publisher.objects.filter(**kwargs).count())
+                        count=Publisher.objects.filter(**kwargs).count())
             if not country:
                 self.create(name='brands',
-                  count=Brand.objects.filter(**kwargs).count())
+                            count=Brand.objects.filter(**kwargs).count())
             self.create(name='indicia publishers', country=country,
-              count=IndiciaPublisher.objects.filter(**kwargs).count())
+                        count=IndiciaPublisher.objects.filter(**kwargs)
+                                                      .count())
         else:
             kwargs['language'] = language
 
         self.create(name='series', language=language, country=country,
-          count=Series.objects.filter(is_comics_publication=True,
-                                      **kwargs).count())
+                    count=Series.objects
+                                .filter(is_comics_publication=True, **kwargs)
+                                .count())
         if 'language' in kwargs:
             kwargs['series__language'] = kwargs['language']
             kwargs.pop('language')
@@ -46,15 +48,19 @@ class CountStatsManager(models.Manager):
         kwargs['series__is_comics_publication'] = True
 
         self.create(name='issues', language=language, country=country,
-          count=Issue.objects.filter(variant_of=None, **kwargs).count())
+                    count=Issue.objects.filter(variant_of=None, **kwargs)
+                                       .count())
 
         self.create(name='variant issues', language=language, country=country,
-          count=Issue.objects.filter(**kwargs)\
-                                     .exclude(variant_of=None).count())
+                    count=Issue.objects
+                               .filter(**kwargs).exclude(variant_of=None)
+                               .count())
 
         self.create(name='issue indexes', language=language, country=country,
-          count=Issue.objects.filter(variant_of=None, **kwargs)\
-                     .exclude(is_indexed=INDEXED['skeleton']).count())
+                    count=Issue.objects
+                               .filter(variant_of=None, **kwargs)
+                               .exclude(is_indexed=INDEXED['skeleton'])
+                               .count())
 
         if 'series__language' in kwargs:
             kwargs['issue__series__language'] = kwargs['series__language']
@@ -65,10 +71,10 @@ class CountStatsManager(models.Manager):
         kwargs.pop('series__is_comics_publication')
 
         self.create(name='covers', language=language, country=country,
-          count=Cover.objects.filter(**kwargs).count())
+                    count=Cover.objects.filter(**kwargs).count())
 
         self.create(name='stories', language=language, country=country,
-          count=Story.objects.filter(**kwargs).count())
+                    count=Story.objects.filter(**kwargs).count())
 
     def update_count(self, field, delta, language=None, country=None):
         """
