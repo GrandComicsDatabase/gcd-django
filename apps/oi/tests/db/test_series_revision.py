5a22494ea258c8a4f39c059aa1ac2d83c7ce5fc7
handrews <hha1@cornell.edu>
2016-02-09 19:00:28 -0800

    Fix issues in database series & bond rev tests.
    These fell victim to the:
    "assert some_mock.called_once_with()"
    typo pattern, which always passes.  So some of them were just wrong,
    and others were out of date since SeriesRevision was refactored.
    
    They should all be valid, working, up-to-date tests now.
    
    Also, consistently name the main revision under test "rev" instead of
    letters representing the type, which I've decided are just too cryptic.

--- apps/oi/tests/db/test_series_revision.py
+++ apps/oi/tests/db/test_series_revision.py
@@ -7,145 +7,189 @@ from apps.gcd.models import Publisher
 from apps.oi.models import SeriesRevision, IssueRevision
 
 
+ONE_SERIES_ONLY = {
+    'series': 1,
+    'issues': 0,
+    'variant issues': 0,
+    'issue indexes': 0,
+    'covers': 0,
+    'stories': 0,
+}
+
+UPDATE_ALL = 'apps.oi.models.CountStats.objects.update_all_counts'
+
+
 @pytest.mark.django_db
 def test_create_add_revision(any_added_series_rev, series_add_values,
                              any_adding_changeset):
-    sr = any_added_series_rev
+    rev = any_added_series_rev
 
     for k, v in series_add_values.iteritems():
-        assert getattr(sr, k) == v
+        assert getattr(rev, k) == v
 
-    assert sr.series is None
+    assert rev.series is None
 
-    assert sr.reservation_requested is False
-    assert sr.changeset == any_adding_changeset
-    assert sr.date_inferred is False
+    assert rev.reservation_requested is False
+    assert rev.changeset == any_adding_changeset
+    assert rev.date_inferred is False
 
-    assert sr.source is None
-    assert sr.source_name == 'series'
+    assert rev.source is None
+    assert rev.source_name == 'series'
 
 
 @pytest.mark.django_db
 def test_commit_added_revision(any_added_series_rev, series_add_values,
                                any_adding_changeset, keywords):
-    sr = any_added_series_rev
+    rev = any_added_series_rev
 
-    old_publisher_series_count = sr.publisher.series_count
+    old_publisher_series_count = rev.publisher.series_count
 
     # Simple version of this for mocking.  Real article testing elsewhere.
-    sort_name = sr.name[sr.name.index(' ') + 1:]
-    with mock.patch('apps.oi.models.update_count') as updater, \
+    sort_name = rev.name[rev.name.index(' ') + 1:]
+    with mock.patch(UPDATE_ALL) as updater, \
             mock.patch('apps.oi.models.remove_leading_article') as remover, \
             mock.patch('apps.oi.models.IssueRevision') as ir_class, \
             mock.patch('apps.oi.models.SeriesRevision.save') as save:
-        remover.return_value = sort_name
-        sr.commit_to_display()
-    assert updater.called_once_with('series', 1,
-                                    language=sr.language, country=sr.country)
-    assert save.called_once_with()
 
-    assert sr.series is not None
-    assert sr.source is sr.series
+        remover.return_value = sort_name
+        rev.commit_to_display()
+
+    updater.assert_has_calls([
+        mock.call({}, language=None, country=None, negate=True),
+        mock.call(ONE_SERIES_ONLY,
+                  language=rev.series.language, country=rev.series.country),
+    ])
+    assert updater.call_count == 2
+
+    # We call save once in _adjust_stats() and once in commit_to_display()
+    save.assert_has_calls([mock.call(), mock.call()])
+    assert save.call_count == 2
+
+    assert rev.series is not None
+    assert rev.source is rev.series
 
     for k, v in series_add_values.iteritems():
         if k == 'keywords':
-            kws = [k for k in sr.publisher.keywords.names()]
+            kws = [k for k in rev.publisher.keywords.names()]
             kws.sort()
             assert kws == keywords['list']
         else:
-            assert getattr(sr.series, k) == v
+            assert getattr(rev.series, k) == v
 
-    assert remover.called_once_with(sr.name)
-    assert sr.series.sort_name == sort_name
+    remover.assert_called_once_with(rev.name)
+    assert rev.series.sort_name == sort_name
 
     # This is needed to get an integer count instead of an F() object.
-    publisher = Publisher.objects.get(pk=sr.publisher.pk)
+    publisher = Publisher.objects.get(pk=rev.publisher.pk)
     assert publisher.series_count == old_publisher_series_count + 1
-    assert sr.series.issue_count == 0
+    assert rev.series.issue_count == 0
 
-    assert ir_class.called == 0
+    assert not ir_class.called
 
 
 @pytest.mark.django_db
 def test_commit_add_rev_no_leading_article(any_added_series_rev):
-    sr = any_added_series_rev
-    sr.leading_article = False
-    sr.save()
+    rev = any_added_series_rev
+    rev.leading_article = False
+    rev.save()
 
-    with mock.patch('apps.oi.models.update_count'), \
+    with mock.patch(UPDATE_ALL), \
             mock.patch('apps.oi.models.remove_leading_article') as remover:
         # Not testing actual remover logic here, just that it's not called.
-        remover.return_value = sr.name
-        sr.commit_to_display()
+        remover.return_value = rev.name
+        rev.commit_to_display()
 
     # Assume everything else worked as tested in the general add case.
     assert remover.called is False
-    assert sr.series.name == sr.name
-    assert sr.series.sort_name == sr.name
+    assert rev.series.name == rev.name
+    assert rev.series.sort_name == rev.name
 
 
 @pytest.mark.django_db
 def test_commit_add_rev_singleton(any_added_series_rev):
-    sr = any_added_series_rev
-    sr.is_singleton = True
-    sr.save()
+    rev = any_added_series_rev
+    rev.is_singleton = True
+    rev.save()
 
-    with mock.patch('apps.oi.models.update_count') as updater, \
+    with mock.patch(UPDATE_ALL) as updater, \
             mock.patch('apps.oi.models.IssueRevision') as ir_class:
         ir = mock.MagicMock(spec=IssueRevision)
         ir_class.return_value = ir
 
-        sr.commit_to_display()
+        rev.commit_to_display()
 
-    assert updater.called_once_with('series', 1,
-                                    language=sr.language, country=sr.country)
-    ir_class.assert_called_once_with(changeset=sr.changeset,
-                                     series=sr.series,
+    updater.assert_has_calls([
+        mock.call({}, language=None, country=None, negate=True),
+        mock.call(ONE_SERIES_ONLY,
+                  language=rev.series.language, country=rev.series.country),
+    ])
+    assert updater.call_count == 2
+
+    # We know that the series revision save() was called if the issue revision
+    # construction happens- otherwise there woulnd't be an rev.series to use.
+    ir_class.assert_called_once_with(changeset=rev.changeset,
+                                     series=rev.series,
                                      after=None,
                                      number='[nn]',
-                                     publication_date=sr.year_began)
-    assert ir.key_date == '%d-00-00' % sr.year_began
+                                     publication_date=rev.year_began)
+    assert ir.key_date == '%d-00-00' % rev.year_began
     ir.save.assert_called_once_with()
+    ir.commit_to_display.assert_called_once_with()
 
 
 @pytest.mark.django_db
 def test_commit_add_rev_non_comics(any_added_series_rev):
-    sr = any_added_series_rev
-    sr.is_comics_publication = False
-    sr.save()
+    rev = any_added_series_rev
+    rev.is_comics_publication = False
+    rev.save()
 
-    old_publisher_series_count = sr.publisher.series_count
+    old_publisher_series_count = rev.publisher.series_count
 
-    with mock.patch('apps.oi.models.update_count') as updater:
-        sr.commit_to_display()
+    with mock.patch(UPDATE_ALL) as updater, \
+            mock.patch('apps.oi.models.IssueRevision') as ir_class:
+        rev.commit_to_display()
 
-    assert updater.called == 0
-    new_publisher = Publisher.objects.get(pk=sr.publisher.pk)
+    updater.assert_has_calls([
+        mock.call({}, language=None, country=None, negate=True),
+        mock.call({'covers': 0, 'stories': 0},
+                  language=rev.series.language, country=rev.series.country),
+    ])
+    assert updater.call_count == 2
+
+    new_publisher = Publisher.objects.get(pk=rev.publisher.pk)
     assert new_publisher.series_count == old_publisher_series_count
+
+    assert not ir_class.called
 
 
 @pytest.mark.django_db
 def test_commit_add_rev_non_comics_singleton(any_added_series_rev):
-    sr = any_added_series_rev
-    sr.is_comics_publication = False
-    sr.is_singleton = True
+    rev = any_added_series_rev
+    rev.is_comics_publication = False
+    rev.is_singleton = True
 
-    with mock.patch('apps.oi.models.update_count') as updater, \
+    with mock.patch(UPDATE_ALL) as updater, \
             mock.patch('apps.oi.models.IssueRevision') as ir_class:
         ir = mock.MagicMock(spec=IssueRevision)
         ir_class.return_value = ir
 
-        sr.commit_to_display()
+        rev.commit_to_display()
 
-    assert updater.called == 0
+    updater.assert_has_calls([
+        mock.call({}, language=None, country=None, negate=True),
+        mock.call({'covers': 0, 'stories': 0},
+                  language=rev.series.language, country=rev.series.country),
+    ])
+    assert updater.call_count == 2
 
-    ir_class.assert_called_once_with(changeset=sr.changeset,
-                                     series=sr.series,
+    ir_class.assert_called_once_with(changeset=rev.changeset,
+                                     series=rev.series,
                                      after=None,
                                      number='[nn]',
-                                     publication_date=sr.year_began)
-    assert ir.key_date == '%d-00-00' % sr.year_began
+                                     publication_date=rev.year_began)
+    assert ir.key_date == '%d-00-00' % rev.year_began
     ir.save.assert_called_once_with()
+    ir.commit_to_display.assert_called_once_with()
 
 
 @pytest.mark.django_db
@@ -155,17 +199,17 @@ def test_create_edit_revision(any_added_series, series_add_values,
     sort_name = any_added_series.name[any_added_series.name.index(' ') + 1:]
     with mock.patch('apps.oi.models.remove_leading_article') as remover:
         remover.return_value = sort_name
-        sr = SeriesRevision.clone(data_object=any_added_series,
-                                  changeset=any_editing_changeset)
+        rev = SeriesRevision.clone(data_object=any_added_series,
+                                   changeset=any_editing_changeset)
 
     for k, v in series_add_values.iteritems():
-        assert getattr(sr, k) == v
+        assert getattr(rev, k) == v
 
-    assert sr.series is any_added_series
+    assert rev.series is any_added_series
 
-    assert sr.reservation_requested is False
-    assert sr.changeset == any_editing_changeset
-    assert sr.date_inferred is False
+    assert rev.reservation_requested is False
+    assert rev.changeset == any_editing_changeset
+    assert rev.date_inferred is False
 
-    assert sr.source is any_added_series
-    assert sr.source_name == 'series'
+    assert rev.source is any_added_series
+    assert rev.source_name == 'series'
--- apps/oi/tests/db/test_seriesbond_revision.py
+++ apps/oi/tests/db/test_seriesbond_revision.py
@@ -11,78 +11,78 @@ from apps.oi.models import SeriesBondRevision
 @pytest.mark.django_db
 def test_create_add_revision(any_added_series_bond_rev, series_bond_add_values,
                              any_adding_changeset):
-    sbr = any_added_series_bond_rev
+    rev = any_added_series_bond_rev
 
     for k, v in series_bond_add_values.iteritems():
-        assert getattr(sbr, k) == v
+        assert getattr(rev, k) == v
 
-    assert sbr.series_bond is None
-    assert sbr.previous_revision is None
-    assert sbr.changeset == any_adding_changeset
+    assert rev.series_bond is None
+    assert rev.previous_revision is None
+    assert rev.changeset == any_adding_changeset
 
-    assert sbr.source is None
-    assert sbr.source_name == 'series_bond'
-    assert sbr.source_class is SeriesBond
+    assert rev.source is None
+    assert rev.source_name == 'series_bond'
+    assert rev.source_class is SeriesBond
 
 
 @pytest.mark.django_db
 def test_commit_added_revision(any_added_series_bond_rev,
                                series_bond_add_values,
                                any_adding_changeset):
-    sbr = any_added_series_bond_rev
+    rev = any_added_series_bond_rev
 
     with mock.patch('apps.oi.models.SeriesBondRevision.save') as mock_save:
-        sbr.commit_to_display()
+        rev.commit_to_display()
 
-    assert mock_save.called_once_with()
-    assert sbr.source is sbr.series_bond
+    mock_save.assert_called_once_with()
+    assert rev.source is rev.series_bond
 
     for k, v in series_bond_add_values.iteritems():
-        assert getattr(sbr.series_bond, k) == v
+        assert getattr(rev.series_bond, k) == v
 
 
 @pytest.mark.django_db
 def test_create_edit_revision(any_added_series_bond, series_bond_add_values,
                               any_added_series_bond_rev,
                               any_editing_changeset):
-    sbr = SeriesBondRevision.clone(data_object=any_added_series_bond,
+    rev = SeriesBondRevision.clone(data_object=any_added_series_bond,
                                    changeset=any_editing_changeset)
 
-    assert sbr.previous_revision == any_added_series_bond_rev
+    assert rev.previous_revision == any_added_series_bond_rev
     for k, v in series_bond_add_values.iteritems():
-        assert getattr(sbr, k) == v
+        assert getattr(rev, k) == v
 
-    assert sbr.series_bond is any_added_series_bond
-    assert sbr.changeset == any_editing_changeset
+    assert rev.series_bond is any_added_series_bond
+    assert rev.changeset == any_editing_changeset
 
-    assert sbr.source is any_added_series_bond
+    assert rev.source is any_added_series_bond
 
 
 @pytest.mark.django_db
 def test_delete_series_bond(any_added_series_bond, any_deleting_changeset,
                             any_added_series_bond_rev):
-    sbr = SeriesBondRevision.clone(data_object=any_added_series_bond,
+    rev = SeriesBondRevision.clone(data_object=any_added_series_bond,
                                    changeset=any_deleting_changeset)
 
-    assert sbr.previous_revision == any_added_series_bond_rev
-    series_bond_id = sbr.series_bond_id
-    sbr.deleted = True
+    assert rev.previous_revision == any_added_series_bond_rev
+    series_bond_id = rev.series_bond_id
+    rev.deleted = True
 
     # TODO: Pre-refactor code relies on the revision having been saved
     #       at least once before committing.  Take this out after refactor?
-    sbr.save()
-    sbr.refresh_from_db()
+    rev.save()
+    rev.refresh_from_db()
 
     # We do not mock out the saving in this case because we want to
     # see that the revision unhook actually made it through to the database.
-    sbr.commit_to_display()
+    rev.commit_to_display()
 
     # Ensure new read of saved row.
     # refresh_from_db apparently does not refresh all related objects.
-    sbr.refresh_from_db()
-    sbr.previous_revision.refresh_from_db()
+    rev.refresh_from_db()
+    rev.previous_revision.refresh_from_db()
 
-    assert sbr.deleted is True
-    assert sbr.series_bond is None
-    assert sbr.previous_revision.series_bond is None
+    assert rev.deleted is True
+    assert rev.series_bond is None
+    assert rev.previous_revision.series_bond is None
     assert SeriesBond.objects.filter(id=series_bond_id).count() == 0
