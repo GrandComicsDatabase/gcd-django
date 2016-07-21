88303b0b071ecd4da937b17e10160c96e301b890
Jochen Garcke <gcdmail@garcke.de>
2016-07-21 23:00:45 +0200

    new bond type

--- apps/gcd/templatetags/display.py
+++ apps/gcd/templatetags/display.py
@@ -19,7 +19,7 @@ from apps.gcd.models.series import Series
 from apps.gcd.models.issue import Issue
 from apps.gcd.models.cover import Cover
 from apps.gcd.models.image import Image
-from apps.gcd.models.seriesbond import SeriesBond, BOND_TRACKING
+from apps.gcd.models.seriesbond import SeriesBond, BOND_TRACKING, SUBNUMBER_TRACKING
 from apps.gcd.views.covers import get_image_tag
 
 register = template.Library()
@@ -186,7 +186,10 @@ def show_series_tracking(series):
             # Wait, why are we here?  Should we assert on this?
             continue
 
-        tracking_line += '<li> numbering continues '
+        if srbond.bond.bond_type.id == SUBNUMBER_TRACKING:
+            tracking_line += '<li> subnumbering continues '
+        else:
+            tracking_line += '<li> numbering continues '
         if (srbond.near_issue != srbond.near_issue_default):
             tracking_line += '%s %s ' % (
                 near_issue_preposition, srbond.near_issue.display_number)
