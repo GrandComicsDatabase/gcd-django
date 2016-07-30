b66f09eedcf9921f004d0c90be134d41a1013c84
handrews <hha1@cornell.edu>
2016-07-30 00:24:43 -0700

    Split up the forms module further.
    No changes to code other than import statements, just moving things
    into different files.

---
+++ apps/oi/forms/image.py
@@ -0,0 +1,138 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from collections import OrderedDict
+
+from django import forms
+from django.core import urlresolvers
+
+from .support import _get_comments_form_field, HiddenInputWithHelp
+
+
+def get_cover_revision_form(revision=None, user=None):
+    compare_url = '<a href="' + urlresolvers.reverse('compare',
+      kwargs={'id': revision.changeset.id}) + '">Compare Change</a>'
+
+    class UploadScanCommentForm(forms.ModelForm):
+        comments = forms.CharField(widget=forms.Textarea,
+                                   required=False,
+          help_text='Comments between the Indexer and Editor about the change. '
+                    'These comments are part of the public change history, but '
+                    'are not part of the regular display. <p>%s</p>'
+                    % compare_url)
+
+        def clean(self):
+            cd = self.cleaned_data
+            cd['comments'] = cd['comments'].strip()
+            return cd
+
+    return UploadScanCommentForm
+
+
+class UploadScanForm(forms.Form):
+    """ Form for cover uploads. """
+
+    scan = forms.ImageField(widget=forms.FileInput)
+    source = forms.CharField(label='Source', required=True,
+      help_text='If you upload a scan from another website, make sure you '
+                'have permission to do that and mention the source. If you '
+                'upload on behalf of someone else you can mention this here as'
+                ' well. Otherwise, indicate that you scanned it yourself.')
+    remember_source = forms.BooleanField(label='Remember the source',
+                                         required=False,
+      help_text="Tick only if you do multiple uploads from the source.")
+    marked = forms.BooleanField(label="Mark cover for replacement", required=False,
+      help_text='Uploads of sub-standard scans for older and/or rare comics '
+                'are fine, but please mark them for replacement.')
+    is_wraparound = forms.BooleanField(label="Wraparound cover",
+                                       required=False,
+      help_text='Cover is a standard wraparound cover: Two pages in width, '
+                'the right half is the front cover and will be automatically '
+                'selected.')
+    is_gatefold = forms.BooleanField(label="Gatefold cover",
+                                         required=False,
+      help_text='Cover is a non-standard wraparound cover or a gatefold cover. '
+                'After the upload you will select the front cover part. '
+                'The selection will not work if JavaScript is turned off.')
+    comments = _get_comments_form_field()
+
+    def clean(self):
+        cd = self.cleaned_data
+        if 'source' in cd:
+            cd['source'] = cd['source'].strip()
+        cd['comments'] = cd['comments'].strip()
+        if cd['is_wraparound'] and cd['is_gatefold']:
+            raise forms.ValidationError(
+              ['Wraparound cover and gatefold cover cannot be both selected. '
+               'A cover that is both wraparound and gatefold should be '
+               'submitted as a gatefold cover.'])
+        return cd
+
+
+class UploadVariantScanForm(UploadScanForm):
+    def __init__(self, *args, **kwargs):
+        super(UploadVariantScanForm, self).__init__(*args, **kwargs)
+        ordering = self.fields.keys()
+        ordering.remove('variant_name')
+        ordering.remove('variant_artwork')
+        ordering.remove('reservation_requested')
+        ordering = ['variant_artwork'] + ordering
+        ordering = ['variant_name'] + ordering
+        ordering = ['reservation_requested'] + ordering
+        new_fields = OrderedDict([(f, self.fields[f]) for f in ordering])
+        self.fields = new_fields
+
+    is_gatefold = forms.CharField(widget=HiddenInputWithHelp, required=False,
+      label="Gatefold cover",
+      help_text='Gatefold uploads are currently not supported when creating '
+        'a variant with a cover image upload. Please first create the '
+        'variant issue before uploading a gatefold cover.')
+    variant_name = forms.CharField(required=False,
+      help_text='Name of this variant. Examples are: "Cover A" (if listed as '
+        'such in the issue), "second printing", "newsstand", "direct", or the '
+        'name of the artist if different from the base issue.')
+    variant_artwork = forms.BooleanField(required=False, initial=True,
+      label='Variant artwork',
+      help_text='Check this box if the uploaded variant cover has artwork '
+        'different from the base issue. If checked a cover sequence will '
+        'be generated with question marks in the creator fields on approval.')
+    reservation_requested = forms.BooleanField(required=False,
+      label='Request variant reservation',
+      help_text='Ideally you request a reservation for the new variant to later'
+        ' fill in the missing data. Check this box to have the variant issue '
+        'reserved to you automatically when it is approved.')
+
+
+class GatefoldScanForm(forms.Form):
+    """ Form for cover uploads. """
+
+    left = forms.IntegerField(widget=forms.TextInput(attrs={'size': '4'}))
+    width = forms.IntegerField(widget=forms.TextInput(attrs={'size': '4'}))
+    real_width = forms.IntegerField(widget=forms.HiddenInput)
+    top = forms.IntegerField(widget=forms.TextInput(attrs={'size': '4'}))
+    height = forms.IntegerField(widget=forms.TextInput(attrs={'size': '4'}))
+    cover_id = forms.IntegerField(widget=forms.HiddenInput, required=False)
+    issue_id = forms.IntegerField(widget=forms.HiddenInput)
+    scan_name = forms.CharField(widget=forms.HiddenInput)
+    source = forms.CharField(widget=forms.HiddenInput, required=False)
+    remember_source = forms.BooleanField(widget=forms.HiddenInput,
+                                         required=False)
+    marked = forms.BooleanField(widget=forms.HiddenInput, required=False)
+    comments = forms.CharField(widget=forms.HiddenInput, required=False)
+
+
+class UploadImageForm(forms.Form):
+    """ Form for image uploads. """
+
+    image = forms.ImageField(widget=forms.FileInput)
+
+    marked = forms.BooleanField(label="Mark image for replacement", required=False,
+      help_text='Uploads of sub-standard images for older and/or rare comics '
+                'are fine, but please mark them for replacement.')
+
+    comments = forms.CharField(widget=forms.Textarea,
+                               required=False,
+                               label='Source / Comments',
+      help_text='Please credit the source of this image if scanned or '
+                'otherwise provided by another site or person, along with '
+                'any additional comments to the upload approver.')
---
+++ apps/oi/forms/publisher.py
@@ -0,0 +1,326 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from collections import OrderedDict
+
+from django import forms
+from django.contrib.admin.widgets import FilteredSelectMultiple
+
+from .support import (
+    _get_comments_form_field, _set_help_labels, _clean_keywords,
+    GENERIC_ERROR_MESSAGE, PUBLISHER_HELP_LINKS, PUBLISHER_HELP_TEXTS,
+    INDICIA_PUBLISHER_HELP_LINKS, BRAND_HELP_LINKS)
+
+from apps.oi.models import (
+    PublisherRevision, IndiciaPublisherRevision, BrandGroupRevision,
+    BrandRevision, BrandUseRevision, get_brand_use_field_list)
+
+from apps.gcd.models import Country, BrandGroup
+
+
+def get_publisher_revision_form(source=None, user=None):
+    class RuntimePublisherRevisionForm(PublisherRevisionForm):
+        if source is not None:
+            # Don't allow country to be un-set:
+            if source.country.code == 'xx':
+                country_queryset = Country.objects.all()
+            else:
+                country_queryset = Country.objects.exclude(code='xx')
+            country = forms.ModelChoiceField(queryset=country_queryset,
+                                             empty_label=None)
+
+        def as_table(self):
+            if not user or user.indexer.show_wiki_links:
+                _set_help_labels(self, PUBLISHER_HELP_LINKS)
+            return super(RuntimePublisherRevisionForm, self).as_table()
+    return RuntimePublisherRevisionForm
+
+
+def _get_publisher_fields(middle=None):
+    first = ['name', 'year_began', 'year_began_uncertain',
+                     'year_ended', 'year_ended_uncertain']
+    last = ['url', 'notes', 'keywords']
+    if middle is not None:
+        first.extend(middle)
+    first.extend(last)
+    return first
+
+
+class PublisherRevisionForm(forms.ModelForm):
+    class Meta:
+        model = PublisherRevision
+        fields = _get_publisher_fields(middle=('country',))
+        widgets = {'name': forms.TextInput(attrs={'autofocus': ''})}
+        help_texts = PUBLISHER_HELP_TEXTS
+
+    country = forms.ModelChoiceField(queryset=Country.objects.exclude(code='xx'))
+    comments = _get_comments_form_field()
+
+    def clean_keywords(self):
+        return _clean_keywords(self.cleaned_data)
+
+    def clean(self):
+        cd = self.cleaned_data
+        if self._errors:
+            raise forms.ValidationError(GENERIC_ERROR_MESSAGE)
+        if 'name' in cd:
+            cd['name'] = cd['name'].strip()
+        cd['notes'] = cd['notes'].strip()
+        cd['comments'] = cd['comments'].strip()
+        return cd
+
+
+def get_indicia_publisher_revision_form(source=None, user=None):
+    class RuntimeIndiciaPublisherRevisionForm(IndiciaPublisherRevisionForm):
+        if source is not None:
+            # Don't allow country to be un-set:
+            country = forms.ModelChoiceField(empty_label=None,
+              queryset=Country.objects.exclude(code='xx'))
+
+        def as_table(self):
+            if not user or user.indexer.show_wiki_links:
+                _set_help_labels(self, INDICIA_PUBLISHER_HELP_LINKS)
+            return super(RuntimeIndiciaPublisherRevisionForm, self).as_table()
+
+    return RuntimeIndiciaPublisherRevisionForm
+
+
+class IndiciaPublisherRevisionForm(PublisherRevisionForm):
+    class Meta(PublisherRevisionForm.Meta):
+        model = IndiciaPublisherRevision
+        fields = _get_publisher_fields(middle=('is_surrogate', 'country'))
+
+    name = forms.CharField(widget=forms.TextInput(attrs={'autofocus': ''}),
+      max_length=255, required=True,
+      help_text='The name exactly as it appears in the indicia or colophon, '
+                'including punctuation, abbreviations, suffixes like ", Inc.",'
+                ' etc. Do not move articles to the end of the name.')
+
+    is_surrogate = forms.BooleanField(required=False, label='Surrogate',
+      help_text='Check if this was an independent company serving as a surrogate '
+                'for the master publisher, rather than a company belonging to the '
+                'master publisher.')
+
+    def clean_keywords(self):
+        return _clean_keywords(self.cleaned_data)
+
+    def clean(self):
+        cd = self.cleaned_data
+        if self._errors:
+            raise forms.ValidationError(GENERIC_ERROR_MESSAGE)
+        if 'name' in cd:
+            cd['name'] = cd['name'].strip()
+        cd['notes'] = cd['notes'].strip()
+        cd['comments'] = cd['comments'].strip()
+        return cd
+
+
+def get_brand_group_revision_form(source=None, user=None):
+    class RuntimeBrandGroupRevisionForm(BrandGroupRevisionForm):
+        if source is None:
+            name = forms.CharField(widget=forms.TextInput(attrs={'autofocus': ''}),
+              max_length=255, help_text='The name of the new brand group.')
+
+        def as_table(self):
+            if not user or user.indexer.show_wiki_links:
+                _set_help_labels(self, BRAND_HELP_LINKS)
+            return super(RuntimeBrandGroupRevisionForm, self).as_table()
+    return RuntimeBrandGroupRevisionForm
+
+
+class BrandGroupRevisionForm(forms.ModelForm):
+    class Meta:
+        model = BrandGroupRevision
+        fields = _get_publisher_fields()
+
+    name = forms.CharField(widget=forms.TextInput(attrs={'autofocus': ''}),
+      max_length=255, help_text='The name of the brand group.')
+
+    year_began = forms.IntegerField(required=False,
+      help_text='The first year the brand group was used.')
+    year_began_uncertain = forms.BooleanField(required=False,
+      help_text='Check if you are not certain of the first year the brand group '
+                'was used.')
+
+    year_ended = forms.IntegerField(required=False,
+      help_text='The last year the brand group was used.  Leave blank if currently '
+                'in use.')
+    year_ended_uncertain = forms.BooleanField(required=False,
+      help_text='Check if you are not certain of the last year the brand group '
+                'was used, or if you are not certain whether it is still in use.')
+
+    url = forms.URLField(required=False,
+      help_text='The official web site of the brand.  Leave blank if the '
+                'publisher does not have a specific web site for the brand group.')
+
+    comments = _get_comments_form_field()
+
+    def clean_keywords(self):
+        return _clean_keywords(self.cleaned_data)
+
+    def clean(self):
+        cd = self.cleaned_data
+        if self._errors:
+            raise forms.ValidationError(GENERIC_ERROR_MESSAGE)
+        if 'name' in cd:
+            cd['name'] = cd['name'].strip()
+        cd['notes'] = cd['notes'].strip()
+        cd['comments'] = cd['comments'].strip()
+        return cd
+
+
+def get_brand_revision_form(source=None, user=None, revision=None,
+                            brand_group=None, publisher=None):
+    initial = []
+    groups = None
+
+    if revision and revision.source:
+        publishers = revision.source.in_use.all().values_list('publisher_id',
+                                                              flat=True)
+        groups = BrandGroup.objects.filter(parent__in=publishers, deleted=False)
+    elif publisher:
+        groups = BrandGroup.objects.filter(parent=publisher.id, deleted=False)
+
+    if groups:
+        choices = [[g.id, g] for g in groups]
+    elif brand_group:
+        initial = [brand_group.id]
+        choices = [[brand_group.id, brand_group]]
+    else:
+        choices = []
+
+    if revision and revision.group.count():
+        for group in revision.group.all():
+            if [group.id, group] not in choices:
+                choices.append([group.id, group])
+
+    class RuntimeBrandRevisionForm(BrandRevisionForm):
+        def __init__(self, *args, **kw):
+            super(BrandRevisionForm, self).__init__(*args, **kw)
+            # brand_group_other_publisher_id is last field, move it after group
+            ordering = self.fields.keys()
+            ordering.insert(ordering.index('group') + 1,
+                            ordering.pop())
+            new_fields = OrderedDict([(f, self.fields[f]) for f in ordering])
+            self.fields = new_fields
+
+        group = forms.MultipleChoiceField(required=True,
+            widget=FilteredSelectMultiple('Brand Groups', False),
+            choices=choices, initial=initial)
+        # maybe only allow editors this to be less confusing to normal indexers
+        brand_group_other_publisher_id = forms.IntegerField(required=False,
+          label="Add Brand Group",
+          help_text="One can add a brand group from a different publisher by "
+            "id. If an id is entered the submit will return for confirmation.")
+
+        def as_table(self):
+            if not user or user.indexer.show_wiki_links:
+                _set_help_labels(self, BRAND_HELP_LINKS)
+            return super(RuntimeBrandRevisionForm, self).as_table()
+    return RuntimeBrandRevisionForm
+
+
+class BrandRevisionForm(forms.ModelForm):
+    class Meta:
+        model = BrandRevision
+        fields = _get_publisher_fields(middle=('group',))
+
+    name = forms.CharField(widget=forms.TextInput(attrs={'autofocus': ''}),
+      max_length=255,
+      help_text='The name of the brand emblem as it appears on the logo.  If '
+                'the logo does not use words, then the name of the brand as '
+                'it is commonly used.  Consult an editor if in doubt.')
+
+    year_began = forms.IntegerField(required=False,
+      help_text='The first year the brand emblem was used.')
+    year_began_uncertain = forms.BooleanField(required=False,
+      help_text='Check if you are not certain of the first year the brand '
+                'emblem was used.')
+
+    year_ended = forms.IntegerField(required=False,
+      help_text='The last year the brand emblem was used.  Leave blank if '
+                'currently in use.')
+    year_ended_uncertain = forms.BooleanField(required=False,
+      help_text='Check if you are not certain of the last year the brand emblem '
+                'was used, or if you are not certain whether it is still in use.')
+
+    url = forms.URLField(required=False,
+      help_text='The official web site of the brand.  Leave blank if the '
+                'publisher does not have a specific web site for the brand.')
+
+    comments = _get_comments_form_field()
+
+    def clean_keywords(self):
+        return _clean_keywords(self.cleaned_data)
+
+    def clean(self):
+        cd = self.cleaned_data
+        if self._errors:
+            raise forms.ValidationError(GENERIC_ERROR_MESSAGE)
+        if 'name' in cd:
+            cd['name'] = cd['name'].strip()
+        cd['notes'] = cd['notes'].strip()
+        cd['comments'] = cd['comments'].strip()
+        if cd['brand_group_other_publisher_id']:
+            brand_group = BrandGroup.objects.filter(
+              id=cd['brand_group_other_publisher_id'], deleted=False)
+            if brand_group:
+                brand_group = brand_group[0]
+                # need to add directly to revision, otherwise validation fails
+                self.instance.group.add(brand_group)
+                choices = self.fields['group'].choices
+                choices.append([brand_group.id, brand_group])
+                # self.data is immutable, need copy
+                data = self.data.copy()
+                data['brand_group_other_publisher_id'] = None
+                self.data = data
+                # need to update with new choices
+                self.fields['group'] = forms.MultipleChoiceField(required=True,
+                    widget=FilteredSelectMultiple('Brand Groups', False),
+                    choices=choices)
+                # TODO maybe do this differently
+                raise forms.ValidationError(
+                  "Please confirm selection of brand group '%s'." % brand_group)
+            else:
+                raise forms.ValidationError(
+                  "A brand group with id %d does not exist." %
+                  cd['brand_group_other_publisher_id'])
+        return cd
+
+
+def get_brand_use_revision_form(source=None, user=None):
+    class RuntimeBrandUseRevisionForm(BrandUseRevisionForm):
+        def as_table(self):
+            if not user or user.indexer.show_wiki_links:
+                _set_help_labels(self, BRAND_HELP_LINKS)
+            return super(RuntimeBrandUseRevisionForm, self).as_table()
+    return RuntimeBrandUseRevisionForm
+
+
+class BrandUseRevisionForm(forms.ModelForm):
+    class Meta:
+        model = BrandUseRevision
+        fields = get_brand_use_field_list()
+
+    year_began = forms.IntegerField(required=False,
+      help_text='The first year the brand was at the publisher.')
+    year_began_uncertain = forms.BooleanField(required=False,
+      help_text='Check if you are not certain of the first year the brand '
+                'was used.')
+
+    year_ended = forms.IntegerField(required=False,
+      help_text='The last year the brand was used at the publisher. '
+                ' Leave blank if currently in use.')
+    year_ended_uncertain = forms.BooleanField(required=False,
+      help_text='Check if you are not certain of the last year the brand '
+                'was used, or if you are not certain whether it is still in use.')
+
+    comments = _get_comments_form_field()
+
+    def clean(self):
+        cd = self.cleaned_data
+        if self._errors:
+            raise forms.ValidationError(GENERIC_ERROR_MESSAGE)
+        cd['notes'] = cd['notes'].strip()
+        cd['comments'] = cd['comments'].strip()
+        return cd
