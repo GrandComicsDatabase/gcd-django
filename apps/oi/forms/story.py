7535c26cae38233482b91febbb126563a3db2c62
handrews <hha1@cornell.edu>
2016-07-31 02:24:01 -0700

    PEP 8 fixes for story and reprint forms.

--- apps/oi/forms/story.py
+++ apps/oi/forms/story.py
@@ -38,17 +38,20 @@ class ReprintRevisionForm(forms.ModelForm):
         fields = get_reprint_field_list()
 
 
-def get_story_revision_form(revision=None, user=None, is_comics_publication=True,
+def get_story_revision_form(revision=None, user=None,
+                            is_comics_publication=True,
                             language=None):
     extra = {}
     additional_genres = []
     selected_genres = []
     if revision is not None:
-        # Don't allow blanking out the type field.  However, when its a new store
-        # make indexers consciously choose a type by allowing an empty
-        # initial value.  So only set None if there is an existing story revision.
+        # Don't allow blanking out the type field.  However, when its a
+        # new store make indexers consciously choose a type by allowing
+        # an empty initial value.  So only set None if there is an existing
+        # story revision.
         extra['empty_label'] = None
-        if revision.issue and revision.issue.series.is_comics_publication is False:
+        if (revision.issue and
+                revision.issue.series.is_comics_publication is False):
             is_comics_publication = False
         if revision.genre:
             genres = revision.genre.split(';')
@@ -77,23 +80,25 @@ def get_story_revision_form(revision=None, user=None, is_comics_publication=True
             queryset = queryset | StoryType.objects.filter(id=revision.type.id)
 
     else:
-        special_types = ['(backcovers) *do not use* / *please fix*', 'filler'] \
-                        + [i for i in OLD_TYPES]
+        special_types = ['(backcovers) *do not use* / *please fix*', 'filler']
+        special_types.extend([i for i in OLD_TYPES])
         queryset = StoryType.objects.all()
         if revision is None or (revision is not None and
                                 revision.type.name not in special_types):
             queryset = queryset.exclude(name__in=special_types)
 
     class RuntimeStoryRevisionForm(StoryRevisionForm):
-        type = forms.ModelChoiceField(queryset=queryset,
-          **extra)
+        type = forms.ModelChoiceField(queryset=queryset, **extra)
 
         fantasy_id = GENRES['en'].index(u'fantasy')
         if language and language.code != 'en' and language.code in GENRES:
-            choices = [[g, g + ' / ' + h] for g, h in zip(GENRES['en'],
-                                                          GENRES[language.code])]
-            choices[fantasy_id] = [u'fantasy',
-              u'fantasy-supernatural / %s' % GENRES[language.code][fantasy_id]]
+            choices = [[g, g + ' / ' + h]
+                       for g, h in zip(GENRES['en'], GENRES[language.code])]
+            choices[fantasy_id] = [
+                u'fantasy',
+                (u'fantasy-supernatural / %s' %
+                 GENRES[language.code][fantasy_id])
+            ]
         else:
             choices = [[g, g] for g in GENRES['en']]
             choices[fantasy_id] = [u'fantasy', u'fantasy-supernatural']
@@ -101,7 +106,8 @@ def get_story_revision_form(revision=None, user=None, is_comics_publication=True
             additional_genres.reverse()
             for genre in additional_genres:
                 choices.insert(0, [genre, genre + ' (deprecated)'])
-        genre = forms.MultipleChoiceField(required=False,
+        genre = forms.MultipleChoiceField(
+            required=False,
             widget=FilteredSelectMultiple('Genres', False),
             choices=choices)
 
@@ -118,7 +124,7 @@ class StoryRevisionForm(forms.ModelForm):
         fields = get_story_field_list()
         fields.insert(fields.index('job_number') + 1, 'creator_help')
         widgets = {
-          'feature': forms.TextInput(attrs={'class': 'wide'}),
+            'feature': forms.TextInput(attrs={'class': 'wide'}),
         }
         help_texts = {
             'keywords':
@@ -132,70 +138,91 @@ class StoryRevisionForm(forms.ModelForm):
     # for new stories we add it through the initial value of a hidden field.
     sequence_number = forms.IntegerField(widget=forms.HiddenInput)
 
-    title = forms.CharField(widget=forms.TextInput(attrs={'class': 'wide',
-                                                          'autofocus': ''}),
-                            required=False,
-      help_text='If no title can be determined, preferably use here'
-                ' the first line of text/dialogue in "quotation marks", or '
-                'a made up title, and check unofficial title.')
-    title_inferred = forms.BooleanField(required=False,
-      label='Unofficial title',
-      help_text='')
+    title = forms.CharField(
+        widget=forms.TextInput(attrs={'class': 'wide', 'autofocus': ''}),
+        required=False,
+        help_text='If no title can be determined, preferably use here'
+                  ' the first line of text/dialogue in "quotation marks", or '
+                  'a made up title, and check unofficial title.')
+    title_inferred = forms.BooleanField(
+        required=False, label='Unofficial title', help_text='')
 
     page_count = forms.DecimalField(widget=PageCountInput, required=False,
                                     max_digits=10, decimal_places=3)
     page_count_uncertain = forms.BooleanField(required=False)
 
-    creator_help = forms.CharField(widget=HiddenInputWithHelp, required=False,
-      help_text='Enter the relevant creator credits in the following fields, '
-                'where multiple credits are separated by semi-colons. Notes '
-                'are in () and aliases in []. If the credit applies to a '
-                'sequence type, but the creator is unknown enter a question '
-                'mark. If a field is not required for a sequence type it can '
-                'be left blank.', label='')
+    creator_help = forms.CharField(
+        widget=HiddenInputWithHelp,
+        required=False,
+        help_text='Enter the relevant creator credits in the following '
+                  'fields, where multiple credits are separated by '
+                  'semi-colons. Notes are in () and aliases in []. If the '
+                  'credit applies to a sequence type, but the creator is '
+                  'unknown enter a question mark. If a field is not required '
+                  'for a sequence type it can be left blank.',
+        label='')
 
     script = forms.CharField(widget=forms.TextInput(attrs={'class': 'wide'}),
                              required=False,
                              help_text='')
     pencils = forms.CharField(widget=forms.TextInput(attrs={'class': 'wide'}),
                               required=False,
-                             help_text='')
+                              help_text='')
     inks = forms.CharField(widget=forms.TextInput(attrs={'class': 'wide'}),
                            required=False,
                            help_text='')
     colors = forms.CharField(widget=forms.TextInput(attrs={'class': 'wide'}),
                              required=False,
                              help_text='')
-    letters = forms.CharField(widget=forms.TextInput(attrs={'class': 'wide'}),
-                              required=False,
-      help_text='If the lettering is produced as normal printed text rather '
-                'than comic-style lettering, enter the word "typeset" here.')
+    letters = forms.CharField(
+        widget=forms.TextInput(attrs={'class': 'wide'}),
+        required=False,
+        help_text='If the lettering is produced as normal printed text rather '
+                  'than comic-style lettering, enter the word "typeset" here.')
     editing = forms.CharField(widget=forms.TextInput(attrs={'class': 'wide'}),
-                             required=False,
-                             help_text='')
+                              required=False,
+                              help_text='')
 
-    no_script = forms.BooleanField(required=False,
-      help_text=NO_CREATOR_CREDIT_HELP % ('script or plot',
-        ', e.g. for a cover or single-page illustration,', 'script'))
-    no_pencils = forms.BooleanField(required=False,
-      help_text=NO_CREATOR_CREDIT_HELP % ('pencils', ', e.g. for '
-                'an unillustrated text article,', 'pencils'))
-    no_inks = forms.BooleanField(required=False,
-      help_text=NO_CREATOR_CREDIT_HELP % ('inks', ', e.g. for a '
-                'story colored straight from pencils,', 'inks'))
-    no_colors = forms.BooleanField(required=False,
-      help_text=NO_CREATOR_CREDIT_HELP % ('colors', ', e.g. for '
-                'a black-and-white comic,', 'colors'))
-    no_letters = forms.BooleanField(required=False,
-      help_text=NO_CREATOR_CREDIT_HELP % ('letters', '', 'letters'))
-    no_editing = forms.BooleanField(required=False,
-      help_text='Check this box if there is no separate editor for this sequence. '
-                'This is common when there is an editor for the whole issue.')
-    synopsis = forms.CharField(widget=forms.Textarea(attrs={'style': 'height: 9em'}),
-                               required=False,
-      help_text='A brief description of the contents. No text under copyright,'
-                ' such as solicitation or other promotional text, may be used'
-                ' without clear permission and credit.')
+    no_script = forms.BooleanField(
+        required=False,
+        help_text=NO_CREATOR_CREDIT_HELP % (
+            'script or plot',
+            ', e.g. for a cover or single-page illustration,',
+            'script'))
+    no_pencils = forms.BooleanField(
+        required=False,
+        help_text=NO_CREATOR_CREDIT_HELP % (
+            'pencils',
+            ', e.g. for an unillustrated text article,',
+            'pencils'))
+    no_inks = forms.BooleanField(
+        required=False,
+        help_text=NO_CREATOR_CREDIT_HELP % (
+            'inks',
+            ', e.g. for a '
+            'story colored straight from pencils,',
+            'inks'))
+    no_colors = forms.BooleanField(
+        required=False,
+        help_text=NO_CREATOR_CREDIT_HELP % (
+            'colors',
+            ', e.g. for '
+            'a black-and-white comic,',
+            'colors'))
+    no_letters = forms.BooleanField(
+        required=False,
+        help_text=NO_CREATOR_CREDIT_HELP % ('letters', '', 'letters'))
+    no_editing = forms.BooleanField(
+        required=False,
+        help_text='Check this box if there is no separate editor for this '
+                  'sequence. This is common when there is an editor for the '
+                  'whole issue.')
+    synopsis = forms.CharField(
+        widget=forms.Textarea(attrs={'style': 'height: 9em'}),
+        required=False,
+        help_text='A brief description of the contents. No text under '
+                  'copyright, such as solicitation or other promotional text, '
+                  'may be used without clear permission and credit.')
     comments = _get_comments_form_field()
 
     def clean_keywords(self):
@@ -249,38 +276,38 @@ class StoryRevisionForm(forms.ModelForm):
 
         if cd['title_inferred'] and cd['title'] == "":
             raise forms.ValidationError(
-              ['Empty titles cannot be unofficial.'])
+                ['Empty titles cannot be unofficial.'])
 
         if cd['title'].startswith('[') and cd['title'].endswith(']'):
             raise forms.ValidationError(
-              ['Do not use [] around unofficial story titles, check the '
-               'unofficial checkbox instead.'])
+                ['Do not use [] around unofficial story titles, check the '
+                 'unofficial checkbox instead.'])
 
         for seq_type in ['script', 'pencils', 'inks', 'colors', 'letters',
                          'editing']:
             if cd['type'].id in NON_OPTIONAL_TYPES:
                 if not cd['no_%s' % seq_type] and cd[seq_type] == "":
                     raise forms.ValidationError(
-                      ['%s field or No %s checkbox must be filled in.' %
-                        (seq_type.capitalize(), seq_type.capitalize())])
+                        ['%s field or No %s checkbox must be filled in.' %
+                         (seq_type.capitalize(), seq_type.capitalize())])
 
             if cd['no_%s' % seq_type] and cd[seq_type] != "":
                 raise forms.ValidationError(
-                  ['%s field and No %s checkbox cannot both be filled in.' %
-                    (seq_type.capitalize(), seq_type.capitalize())])
+                    ['%s field and No %s checkbox cannot both be filled in.' %
+                     (seq_type.capitalize(), seq_type.capitalize())])
 
         if (len(cd['synopsis']) > settings.LIMIT_SYNOPSIS_LENGTH and
-            (self.instance is None or
-             self.instance.synopsis.strip() != cd['synopsis'])):
+                (self.instance is None or
+                 self.instance.synopsis.strip() != cd['synopsis'])):
 
             raise forms.ValidationError(
-              ['The synopsis field may not be longer than %d characters.' %
-                  settings.LIMIT_SYNOPSIS_LENGTH])
+                ['The synopsis field may not be longer than %d characters.' %
+                 settings.LIMIT_SYNOPSIS_LENGTH])
 
         if (cd['page_count'] is None and not cd['page_count_uncertain'] and
                 cd['type'].id != STORY_TYPES['insert']):
             raise forms.ValidationError(
-              ['Page count uncertain must be checked if the page count '
-               'is not filled in.'])
+                ['Page count uncertain must be checked if the page count '
+                 'is not filled in.'])
 
         return cd
