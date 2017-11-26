ac3e3cc7b2bbea4205678fd74ce9e9f48faac497
Jochen Garcke <gcdmail@garcke.de>
2017-11-26 14:30:39 +0100

    add volume_not_printed fix status tables for singletons

---
+++ apps/gcd/migrations/0005_issue_volume_not_printed.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('gcd', '0004_initial_creator_data'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='issue',
+            name='volume_not_printed',
+            field=models.BooleanField(default=False),
+            preserve_default=True,
+        ),
+    ]
