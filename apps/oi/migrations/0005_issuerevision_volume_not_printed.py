3b94d01a801fb678ccbdd1f43ea89e40bd471f80
jochengcd <gcdmail@garcke.de>
2017-12-10 10:30:56 +0100

    volume not printed (#349)
    add volume_not_printed
    fix status tables for singletons

---
+++ apps/oi/migrations/0005_issuerevision_volume_not_printed.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('oi', '0004_creators'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='issuerevision',
+            name='volume_not_printed',
+            field=models.BooleanField(default=False),
+            preserve_default=True,
+        ),
+    ]
