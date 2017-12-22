d117b878d286196ea14816888c1d4c5b79d7a641
jochen.garcke <jochen.garcke@scai.fraunhofer.de>
2017-12-22 11:21:41 +0100

    add Creator to stats, remove Brands and IndiciaPublisher from stats code cleanup in stats

--- apps/stats/models.py
+++ apps/stats/models.py
@@ -4,8 +4,8 @@ from django.conf import settings
 from django.contrib.auth.models import User
 
 from apps.stddata.models import Country, Language
-from apps.gcd.models import Publisher, Brand, IndiciaPublisher, \
-                            Series, Issue, INDEXED, Story, Cover
+from apps.gcd.models import Publisher, Series, Issue, INDEXED, Story, Cover,\
+                            Creator
 
 
 class CountStatsManager(models.Manager):
@@ -15,24 +15,22 @@ class CountStatsManager(models.Manager):
             raise ValueError('either country or language stats')
         self.filter(language=language, country=country).delete()
         if country:
-            kwargs = { 'country': country, 'deleted': False }
+            kwargs = {'country': country, 'deleted': False}
         else:
-            kwargs = { 'deleted': False }
+            kwargs = {'deleted': False}
 
         if language is None:
             self.create(name='publishers', country=country,
-              count=Publisher.objects.filter(**kwargs).count())
+                        count=Publisher.objects.filter(**kwargs).count())
             if not country:
-                self.create(name='brands', 
-                  count=Brand.objects.filter(**kwargs).count())
-            self.create(name='indicia publishers', country=country,
-              count=IndiciaPublisher.objects.filter(**kwargs).count())
+                self.create(name='creators',
+                            count=Creator.objects.filter(**kwargs).count())
         else:
             kwargs['language'] = language
 
         self.create(name='series', language=language, country=country,
-          count=Series.objects.filter(is_comics_publication=True,
-                                      **kwargs).count())
+                    count=Series.objects.filter(is_comics_publication=True,
+                                                **kwargs).count())
         if 'language' in kwargs:
             kwargs['series__language'] = kwargs['language']
             kwargs.pop('language')
@@ -42,15 +40,17 @@ class CountStatsManager(models.Manager):
         kwargs['series__is_comics_publication'] = True
 
         self.create(name='issues', language=language, country=country,
-          count=Issue.objects.filter(variant_of=None, **kwargs).count())
+                    count=Issue.objects.filter(variant_of=None,
+                                               **kwargs).count())
 
         self.create(name='variant issues', language=language, country=country,
-          count=Issue.objects.filter(**kwargs)\
-                                     .exclude(variant_of=None).count())
+                    count=Issue.objects.filter(**kwargs)
+                                       .exclude(variant_of=None).count())
 
         self.create(name='issue indexes', language=language, country=country,
-          count=Issue.objects.filter(variant_of=None, **kwargs)\
-                     .exclude(is_indexed=INDEXED['skeleton']).count())
+                    count=Issue.objects.filter(variant_of=None, **kwargs)
+                                       .exclude(is_indexed=INDEXED['skeleton'])
+                                       .count())
 
         if 'series__language' in kwargs:
             kwargs['issue__series__language'] = kwargs['series__language']
@@ -59,12 +59,12 @@ class CountStatsManager(models.Manager):
             kwargs['issue__series__country'] = kwargs['series__country']
             kwargs.pop('series__country')
         kwargs.pop('series__is_comics_publication')
-            
+
         self.create(name='covers', language=language, country=country,
-          count=Cover.objects.filter(**kwargs).count())
+                    count=Cover.objects.filter(**kwargs).count())
 
         self.create(name='stories', language=language, country=country,
-          count=Story.objects.filter(**kwargs).count())
+                    count=Story.objects.filter(**kwargs).count())
 
 
 class CountStats(models.Model):
@@ -110,7 +110,7 @@ class RecentIndexedIssueManager(models.Manager):
             if count > settings.RECENTS_COUNT:
                 for recent in local.order_by('created')\
                                 [:count - settings.RECENTS_COUNT]:
-                    recent.delete()         
+                    recent.delete()
 
 
 class RecentIndexedIssue(models.Model):
