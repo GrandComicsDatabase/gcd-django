0b180e9809349d0a24b3c596e7d861da68371538
jochen.garcke <gcdmail@garcke.de>
2018-12-18 22:18:32 +0100

    modify contact mail

--- apps/gcd/forms/contact.py
+++ apps/gcd/forms/contact.py
@@ -5,5 +5,5 @@ from captcha.fields import ReCaptchaField
 class CustomContactForm(ContactForm):
     recipient_list = ['%s' % settings.DEFAULT_FROM_EMAIL,
                       'gcd-contact@googlegroups.com']
-    from_email = 'do-not_reply@comics.org'
+    from_email = 'do-not-reply@comics.org'
     captcha = ReCaptchaField()
