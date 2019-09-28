62db43e69996d5440bf25b8c851e03da988ad1a3
jochen.garcke <gcdmail.garcke.de>
2019-09-28 15:19:49 +0200

    CreatorAutocomplete contains

--- apps/select/views.py
+++ apps/select/views.py
@@ -415,7 +415,7 @@ class CreatorAutocomplete(LoginRequiredMixin,
         qs = Creator.objects.filter(deleted=False)
 
         if self.q:
-            qs = qs.filter(sort_name__istartswith=self.q)
+            qs = qs.filter(name__icontains=self.q)
 
         return qs
 
