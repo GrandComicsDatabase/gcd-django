b5fd4bc3aed68b69b85c7654e3e05d70a20af00d
jochen.garcke <gcdmail.garcke.de>
2020-04-18 16:13:00 +0200

    fix wrong python2to3 conversion of viewkeys

--- apps/oi/models.py
+++ apps/oi/models.py
@@ -4799,10 +4799,9 @@ class StoryRevision(Revision):
             if fork is True:
                 source_story = fork_source
             # copy single value fields which are specific to biblio_entry
-            for field in biblio_revision.\
-                           list(_get_single_value_fields()) - \
-                         biblio_revision.storyrevision_ptr.\
-                           list(_get_single_value_fields()):
+            for field in biblio_revision._get_single_value_fields().keys() - \
+                         biblio_revision.storyrevision_ptr.
+                           _get_single_value_fields().keys():
                 setattr(biblio_revision, field,
                         getattr(source_story.biblioentry, field))
             biblio_revision.save()
