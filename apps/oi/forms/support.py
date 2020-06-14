878c4b1e24b9ce82d219ad0a3cf34d86c707a203
jochen.garcke <gcdmail.garcke.de>
2020-06-14 08:43:07 +0200

    style guide

--- apps/oi/forms/support.py
+++ apps/oi/forms/support.py
@@ -332,8 +332,8 @@ SERIES_HELP_TEXTS = {
         'two color, duotone, and black and white. '
         'This may change over the life of the series.',
     'dimensions':
-        'The size of the comic, such as standard Golden Age U.S.'
-        '(or Silver or Modern), A4, A5, tabloid, digest, 8.5" x 11", '
+        'The size of the comic, such as standard Golden Age US'
+        '(or Silver or Modern), A4, A5, tabloid, digest, 8.5in x 11in, '
         '21cm x 28cm.  This may change over the life of the series.',
     'paper_stock':
         'Type of paper used for the interior pages, such as '
