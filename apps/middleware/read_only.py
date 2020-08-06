4d404a01f96914d75d04c4c932e9fe763c5445cf
jochen.garcke <gcdmail@garcke.de>
2020-08-06 10:00:27 +0200

    update read_only MIDDLEWARE

--- apps/middleware/read_only.py
+++ apps/middleware/read_only.py
@@ -1,14 +1,15 @@
 # -*- coding: utf-8 -*-
 from django.contrib.auth import logout
 from apps.indexer.views import render_error
+from django.utils.deprecation import MiddlewareMixin
 
 #
 # read_only mode, keep all users logged out
 #
 
-class ReadOnlyMiddleware:
+class ReadOnlyMiddleware(MiddlewareMixin):
     def process_response(self, request, response): 
-        if request.user.is_authenticated():
+        if request.user.is_authenticated:
             logout(request)
             return render_error(request, "Online editing is currently turned "
                 "off, no user can login. We are working on the site. "
