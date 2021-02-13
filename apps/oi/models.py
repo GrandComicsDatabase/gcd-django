f7de229330bebca066d4dec694b336f9f1637d84
jochen.garcke <gcdmail.garcke.de>
2021-02-13 17:51:19 +0100

    disambiguation is optional

--- apps/oi/models.py
+++ apps/oi/models.py
@@ -7533,7 +7533,7 @@ class CreatorRevision(Revision):
 
     gcd_official_name = models.CharField(max_length=255, db_index=True)
     disambiguation = models.CharField(max_length=255, default='',
-                                      db_index=True)
+                                      db_index=True, blank=True)
 
     # TODO change from null=True
     birth_date = models.ForeignKey(Date, on_delete=models.CASCADE,
