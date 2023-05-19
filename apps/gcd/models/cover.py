a11f77a74fca5bebbbad5f3fa4c21bb7bc9b9c4b
jochenGCD <gcdmail.garcke.de>
2023-05-19 21:57:54 +0200

    series cover overview

--- apps/gcd/models/cover.py
+++ apps/gcd/models/cover.py
@@ -3,10 +3,11 @@ from django.conf import settings
 from django.db import models
 from django.utils.safestring import mark_safe
 import django.urls as urlresolvers
+from django.template import engines
 
 import django_tables2 as tables
 
-from .issue import Issue, IssuePublisherTable
+from .issue import Issue, IssueTable, IssuePublisherTable, IssueColumn
 
 # TODO: should not be importing oi app into gcd app, dependency should be
 # the other way around.  Probably.
@@ -85,23 +86,15 @@ class Cover(models.Model):
         self.save()
 
     def deletable(self):
-        return self.revisions.filter(changeset__state__in=states.ACTIVE).count() == 0
+        return self.revisions.filter(changeset__state__in=states.ACTIVE)\
+                             .count() == 0
 
     def __str__(self):
         return '%s %s cover' % (self.issue.series, self.issue.display_number)
 
 
-class CoverIssuePublisherTable(IssuePublisherTable):
-    cover = tables.Column(accessor='active_covers',
-                          verbose_name='Cover', orderable=False)
-
-    class Meta:
-        model = Issue
-        fields = ('cover', 'publisher', 'issue', 'publication_date',
-                  'on_sale_date')
-        attrs = {'th': {'class': "non_visited"}}
-
-    def render_cover(self, value):
+class CoverColumn(tables.Column):
+    def render(self, value):
         from apps.gcd.views.covers import get_image_tag
         cover_tag = ''
         for cover in value:
@@ -109,3 +102,47 @@ class CoverIssuePublisherTable(IssuePublisherTable):
                                                   get_image_tag(cover,
                                                                 '', 1))
         return mark_safe(cover_tag)
+
+
+class CoverIssuePublisherTable(IssuePublisherTable):
+    cover = CoverColumn(accessor='active_covers',
+                        verbose_name='Cover', orderable=False)
+
+    class Meta:
+        model = Issue
+        fields = ('cover', 'publisher', 'issue', 'publication_date',
+                  'on_sale_date')
+        attrs = {'th': {'class': "non_visited"}}
+
+
+class CoverIssueTable(IssueTable):
+    cover = CoverColumn(accessor='active_covers',
+                        verbose_name='Cover', orderable=False)
+    issue = IssueColumn(accessor='id', verbose_name='Issue',
+                        template_name='gcd/bits/sortable_issue_overview.html',
+                        )
+    longest_story = tables.Column(
+      accessor='longest_story_id', verbose_name='Longest Story',
+      orderable=False)
+
+    class Meta:
+        model = Issue
+        fields = ('cover', 'issue', 'longest_story', 'publication_date',
+                  'on_sale_date')
+        attrs = {'th': {'class': "non_visited"}}
+
+    def render_longest_story(self, value):
+        from .story import Story
+        template_name = 'gcd/bits/story_overview.html'
+
+        story = Story.objects.get(id=value)
+
+        request_context = self.context
+        context = {'story': story,
+                   'ICON_SET_SYMBOLIC': request_context['ICON_SET_SYMBOLIC']}
+
+        django_engine = engines['django']
+        template = django_engine.get_template(template_name)
+        rendered_output = template.render(context)
+
+        return rendered_output
