ebbd498e1185f7ee82ee4fd3b0705a239b4aaaa8
jochenGCD <gcdmail.garcke.de>
2025-01-12 21:59:51 +0100

    align modal-previews on table listing

--- apps/gcd/models/story.py
+++ apps/gcd/models/story.py
@@ -820,7 +820,8 @@ class StoryTable(tables.Table):
     def render_publisher(self, value):
         from apps.gcd.templatetags.display import absolute_url
         from apps.gcd.templatetags.credits import show_country_info
-        display_publisher = "<img %s>" % (show_country_info(value.country))
+        display_publisher = "<img class='px-1 inline' %s>" % (
+          show_country_info(value.country))
         return mark_safe(display_publisher) + absolute_url(value)
 
     def value_publisher(self, value):
