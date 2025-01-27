9904a0feaa2b959dfa50184f23b2ad21df8c3324
jochenGCD <gcdmail.garcke.de>
2025-01-28 00:48:20 +0100

    add transition

--- apps/gcd/templatetags/display.py
+++ apps/gcd/templatetags/display.py
@@ -46,8 +46,9 @@ def absolute_url(item, popup=None, descriptor=''):
         if descriptor == '':
             descriptor = esc(item)
         if popup and not settings.FAKE_IMAGES:
-            image_link = '<span class="image_popup group-hover:visible ' \
-                         'invisible absolute"><img src="%s"></span>' \
+            image_link = '<span class="image_popup opacity-0 ease-in-out ' \
+                         'delay-100 duration-300 absolute transition-opacity' \
+                         ' group-hover:opacity-100"><img src="%s"></span>' \
                          % popup.thumbnail.url
             return mark_safe('<a href="%s" class="group popup">%s%s</a>' %
                              (item.get_absolute_url(), image_link, descriptor))
