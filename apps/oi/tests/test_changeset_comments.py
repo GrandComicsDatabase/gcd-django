caaff9157e23ab9653e2f16e04d3abd4511890cb
Adam Hernandez <adam@Hernandez-Partners.local>
2026-08-23 13:05:26 -0700

    Verify online comment charset migration

--- apps/oi/tests/test_changeset_comments.py
+++ apps/oi/tests/test_changeset_comments.py
@@ -5,7 +5,7 @@
 
 from importlib import import_module
 
-from django.db import migrations
+from django.db import connection, migrations
 
 from apps.oi.forms.support import _get_comments_form_field
 from apps.oi.models import ChangesetComment
@@ -54,3 +54,38 @@ def test_comment_charset_migration_requires_online_ddl():
     assert 'COLLATE utf8mb4_unicode_ci' in operation.sql
     assert 'ALGORITHM=INPLACE' in operation.sql
     assert 'LOCK=NONE' in operation.sql
+
+
+def test_comment_charset_migration_runs_online_on_legacy_mysql_column(db):
+    """The online DDL works when converting a legacy utf8mb3 column."""
+    migration_module = import_module(
+        'apps.oi.migrations.0067_changeset_comment_utf8mb4',
+    )
+    operation = migration_module.Migration.operations[0]
+    table_name = 'issue_629_comment_ddl_probe'
+    sql = operation.sql.replace(
+        '`oi_changeset_comment`',
+        f'`{table_name}`',
+    )
+
+    with connection.cursor() as cursor:
+        cursor.execute(f'DROP TABLE IF EXISTS `{table_name}`')
+        try:
+            cursor.execute(
+                f'CREATE TABLE `{table_name}` ('
+                'text longtext CHARACTER SET utf8mb3 '
+                'COLLATE utf8mb3_unicode_ci NOT NULL'
+                ') ENGINE=InnoDB',
+            )
+            cursor.execute(sql)
+            cursor.execute(
+                'SELECT CHARACTER_SET_NAME, COLLATION_NAME '
+                'FROM information_schema.COLUMNS '
+                'WHERE TABLE_SCHEMA = DATABASE() '
+                'AND TABLE_NAME = %s AND COLUMN_NAME = %s',
+                [table_name, 'text'],
+            )
+
+            assert cursor.fetchone() == ('utf8mb4', 'utf8mb4_unicode_ci')
+        finally:
+            cursor.execute(f'DROP TABLE IF EXISTS `{table_name}`')
