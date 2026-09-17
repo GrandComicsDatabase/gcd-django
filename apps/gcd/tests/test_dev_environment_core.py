6c2348663c8f6d36921d61e5851b6c6b5aa74893
Adam Hernandez <adam@Hernandez-Partners.local>
2026-09-17 12:48:59 -0700

    Require authenticated database readiness

--- apps/gcd/tests/test_dev_environment_core.py
+++ apps/gcd/tests/test_dev_environment_core.py
@@ -68,7 +68,9 @@ def test_compose_healthchecks_quote_credentials_and_remain_readable():
 
     assert services['db']['healthcheck']['test'] == [
         'CMD-SHELL',
-        "mysqladmin ping -h localhost -u'$$MYSQL_USER' -p'$$MYSQL_PASSWORD'",
+        'mysql --protocol=TCP -h localhost -u"$$MYSQL_USER" '
+        '-p"$$MYSQL_PASSWORD" --database="$$MYSQL_DATABASE" '
+        '--execute="SELECT 1" >/dev/null',
     ]
     assert services['web']['healthcheck']['test'] == [
         'CMD-SHELL',
